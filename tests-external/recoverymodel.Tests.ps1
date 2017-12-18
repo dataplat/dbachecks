c62d6d73b5a2d17d20f519573012fe79a2bdba99
potatoqualitee <clemaire@gmail.com>
2017-12-19 00:23:15 +0100

    updated test

--- tests-external/recoverymodel.Tests.ps1
+++ tests-external/recoverymodel.Tests.ps1
@@ -1,8 +1,12 @@
-﻿(Get-DbcConfigValue  SqlInstance).ForEach{
-	Context "Testing recovery model on $psitem" {
-		$results = Get-DbaDbRecoveryModel -SqlInstance $psitem -SqlCredential (Get-DbcConfigValue SqlCredential)
-		foreach ($result in $results) {
-			$result.RecoveryModel -eq 'Full' | Should Be $true
+﻿Describe 'Testing FullRecovery Model' -Tags Backup, Database, DISA {
+	(Get-DbcConfigValue  SqlInstance).ForEach{
+		Context "Testing recovery models for $psitem" {
+			$results = Get-DbaDbRecoveryModel -SqlInstance $psitem -SqlCredential (Get-DbcConfigValue SqlCredential)
+			foreach ($result in $results) {
+				It "$result should be in Full Recovery" {
+					$result.RecoveryModel -eq 'Full' | Should be $true
+				}
+			}
 		}
 	}
 }
