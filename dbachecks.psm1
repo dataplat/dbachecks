5ea8a71e2829872a7ee4fba9095b3ed411170935
potatoqualitee <clemaire@gmail.com>
2017-12-18 20:55:39 +0100

    initial commit

---
+++ dbachecks.psm1
@@ -0,0 +1,12 @@
+# All internal functions privately available within the toolset - for developers - smaller modules may not have any
+# But basically, these are functions that are shared between the exported commands, that you don't nessarily want end users to use
+foreach ($function in (Get-ChildItem "$PSScriptRoot\internal\*.ps1"))
+{
+	$ExecutionContext.InvokeCommand.InvokeScript($false, ([scriptblock]::Create([io.file]::ReadAllText($function))), $null, $null)
+}
+
+# All exported functions - for end users
+foreach ($function in (Get-ChildItem "$PSScriptRoot\functions\*.ps1"))
+{
+	$ExecutionContext.InvokeCommand.InvokeScript($false, ([scriptblock]::Create([io.file]::ReadAllText($function))), $null, $null)
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# dbachecks
+
+Check stuff
+
+## Getting started
+
+Do this
+
+## Party
+
+Nice work!
