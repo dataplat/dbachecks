d23a5fcc85f736aa760b693821a3fe3a642ad79a
potatoqualitee <clemaire@gmail.com>
2017-12-19 02:53:15 +0100

    added new test

--- internal/configurations/configuration.ps1
+++ internal/configurations/configuration.ps1
@@ -1,9 +1,15 @@
-﻿# Can't persist yet
+﻿# setup
 Set-PSFConfig -Module dbachecks -Name setup.sqlinstance -Value $null -Initialize -Description "List of SQL Server instances that SQL-based tests will run against"
 Set-PSFConfig -Module dbachecks -Name setup.computername -Value $null -Initialize -Description "List of Windows Servers that Windows-based tests will run against"
 Set-PSFConfig -Module dbachecks -Name setup.testrepo -Value "$script:ModuleRoot\tests-external" -Initialize -Description "Where tests are stored"
-
-# Persists
 Set-PSFConfig -Module dbachecks -Name setup.sqlcredential -Value $null -Initialize -Description "The universal SQL credential if Trusted/Windows Authentication is not used"
 Set-PSFConfig -Module dbachecks -Name setup.wincredential -Value $null -Initialize -Description "The universal Windows if default Windows Authentication is not used"
+
+# skips
+Set-PSFConfig -Module dbachecks -Name skip.backupdiffcheck -Value $false -Initialize -Description "If you don't use diffs in your enviornment, you can skip this"
+
+# Policy
 Set-PSFConfig -Module dbachecks -Name policy.diskspacepercentfree -Value 20 -Initialize -Description "Percent disk free"
+Set-PSFConfig -Module dbachecks -Name policy.backupfullmaxdays -Value 1 -Initialize -Description "Maxmimum number of days before Full Backups are considered outdated"
+Set-PSFConfig -Module dbachecks -Name policy.backupdiffmaxhours -Value 25 -Initialize -Description "Maxmimum number of hours before Diff Backups are considered outdated"
+Set-PSFConfig -Module dbachecks -Name policy.backuplogmaxminutes -Value 15 -Initialize -Description "Maxmimum number of minutes before Log Backups are considered outdated"
---
+++ tests-external/lastbackup.Tests.ps1
@@ -0,0 +1,30 @@
+﻿$maxfull = Get-DbcConfigValue policy.backupfullmaxdays
+$maxdiff = Get-DbcConfigValue policy.backupdiffmaxhours
+$maxlog  = Get-DbcConfigValue policy.backuplogmaxminutes
+
+Describe 'Testing Last Backup Times' -Tags Backup, Database, DISA {
+	(Get-DbcConfigValue Setup.SqlInstance).ForEach{
+		Context "Testing backups on $psitem" {
+			$results = Get-DbaDatabase -SqlInstance $psitem
+			
+			foreach ($result in $results) {
+			
+				if ($result.RecoveryModel -ne 'Simple') {
+					It "full backups for $result should be less than $maxfull days" {
+						$result.LastFullBackup -ge (Get-Date).AddDays(-($maxfull)) | Should be $true
+					}
+				}
+				
+				if (-not (Get-DbcConfigValue skip.backupdiffcheck)) {
+					It "diff backups for $result should be less than $maxdiff hours" {
+						$result.LastDiffBackup -ge (Get-Date).AddHours(-($maxdiff)) | Should be $true
+					}
+				}
+				
+				It "log backups for $result should be less than $maxlog minutes" {
+					$result.LastLogBackup -ge (Get-Date).AddMinutes(-($maxlog)) | Should be $true
+				}
+			}
+		}
+	}
+}
