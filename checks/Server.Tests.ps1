c571265163856c7591a723fd5232abe6c983e6cc
Rob Sewell <mrrobsewell@outlook.com>
2017-12-20 19:52:24 +0000

    Added PowerPlan Test

---
+++ checks/Server.Tests.ps1
@@ -0,0 +1,10 @@
+$filename = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Describe 'Testing Server PowerPlan Configuration' -Tag PowerPlan, Server $filename {
+    (Get-ComputerName).ForEach{
+        Context "Testing $_" {
+            It "Server PowerPlan should be High Performance" {
+                (Test-DbaPowerPlan -ComputerName $_).IsBestPractice | Should be $true
+            }
+        }
+    }
+}
