f3c1530d9674d25016e41a96d6ea0488cb4103c0
Rob Sewell <mrrobsewell@outlook.com>
2017-12-20 21:44:02 +0000

    added backuptesting to database test

--- checks/Database.Tests.ps1
+++ checks/Database.Tests.ps1
@@ -9,3 +9,24 @@ Describe 'Testing Database Collation' -Tags Database, Collation, $filename {
 		}
 	}
 }
+
+if (-not (Get-DbcConfigValue skip.backuptesting)) {
+	$destserver = Get-DbcConfigValue setup.backuptestserver
+	$destdata = Get-DbcConfigValue setup.backupdatadir
+	$destlog = Get-DbcConfigValue setup.backuplogdir
+	Describe 'Testing backups' -Tags Backup, Database, $filename {
+		(Get-SqlInstance).ForEach{
+			foreach ($result in (Test-DbaLastBackup -SqlInstance $psitem -Destination $destserver -LogDirectory $destlog -DataDirectory $destdata )) {
+				if ($result.DBCCResult -notmatch 'skipped for restored master') {
+					It "DBCC for $($result.Database) on $psitem should be success" {
+						$result.DBCCResult | Should Be 'Success'
+					}
+					It "restore for $($result.Database) on $psitem should be success" {
+						$result.RestoreResult | Should Be 'Success'
+					}
+					
+				}
+			}
+		}
+	}
+}
