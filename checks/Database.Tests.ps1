ed3e1068a3682ff92357d6e54938a92626fd21f2
Rob Sewell <mrrobsewell@outlook.com>
2017-12-20 21:45:30 +0000

    added dbownershould to database tests

--- checks/Database.Tests.ps1
+++ checks/Database.Tests.ps1
@@ -30,3 +30,17 @@ if (-not (Get-DbcConfigValue skip.backuptesting)) {
 		}
 	}
 }
+
+$targetowner = Get-DbcConfigValue policy.dbownershould
+Describe 'Testing Database Owners' -Tags Database, Owner, $filename {
+    (Get-SqlInstance).ForEach{
+        Context "Testing $psitem for Database Owners" {
+            $results = Test-DbaDatabaseOwner -SqlInstance $psitem -TargetLogin $targetowner
+            $results.ForEach{
+                It "$($psitem.Database) Owner should be $targetowner" {
+                    $psitem.CurrentOwner | Should Be $psitem.TargetOwner
+                }
+            }
+        }
+    }
+}
