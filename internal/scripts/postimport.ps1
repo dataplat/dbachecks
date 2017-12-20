2bd4382f6cedae5a3917b8b519e0a81007cb9d95
FriedrichWeinmann <friedrich.weinmann@powershell-stuttgart.de>
2017-12-20 19:58:32 +0100

    Fixed path to relative path

--- internal/scripts/postimport.ps1
+++ internal/scripts/postimport.ps1
@@ -10,7 +10,7 @@ $repo = Get-DbcConfigValue -Name setup.testrepo
 $alltags = (Get-ChildItem -Path "$repo\*Tests.ps1").Name.Replace(".Tests.ps1", "")
 $content = Get-Content "$repo\*Tests.ps1" | Where-Object { $_ -match "-Tag" }
 $regex = [regex]'-Tags[\s]*(.+)[\s]*\$filename'
-$parsedtags = Get-Content C:\github\dbachecks\checks\*.Tests.ps1 | Where-Object { $_ -match "-Tag" } | Select-String -Pattern '-Tags[\s]*(.+)[\s]*\$filename' | ForEach-Object { $_.matches.Groups[1].Value }
+$parsedtags = Get-Content "$ModuleRoot\checks\*.Tests.ps1" | Where-Object { $_ -match "-Tag" } | Select-String -Pattern '-Tags[\s]*(.+)[\s]*\$filename' | ForEach-Object { $_.matches.Groups[1].Value }
 $alltags += $parsedtags.Split(",").Trim() | Where-Object { $_.length -gt 2 } | Select-Object -Unique
 Set-PSFConfig -Module dbachecks -Name autocomplete.pestertags -Value $alltags
 
