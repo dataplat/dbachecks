ca6cd4cb8879ef11d75873ee8c5368c545d8f45d
Rob Sewell <mrrobsewell@outlook.com>
2017-12-20 21:35:41 +0000

    added AdHoc to Instance

--- checks/Instance.Tests.ps1
+++ checks/Instance.Tests.ps1
@@ -21,3 +21,15 @@ Describe 'Testing TempDB Configuration' -Tag TempDB, Instance , $filename {
         }
     }
 }
+
+Describe 'Testing Optimise for AdHoc Workloads setting' -Tag AdHoc, Instance , $filename{
+
+    (Get-SQLInstance).ForEach{
+        Context "Testing $psitem" {
+            It "Should be Optimised for AdHocworkloads" {
+                $Results = Test-DbaOptimizeForAdHoc -SqlInstance $psitem
+                $Results.CurrentOptimizeAdHoc | Should be $Results.RecommendedOptimizeAdHoc
+            }
+        }
+    }
+}
