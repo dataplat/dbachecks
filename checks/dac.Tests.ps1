5f8915385dc7e7ff49fd390cf9b2ba00f15fa3da
Rob Sewell <mrrobsewell@outlook.com>
2017-12-20 19:39:14 +0000

    Added context block per instance

--- checks/dac.Tests.ps1
+++ checks/dac.Tests.ps1
@@ -1,8 +1,10 @@
 $filename = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Describe 'Testing DAC' -Tags DAC, $filename {
     (Get-SqlInstance).ForEach{
-        It "$psitem Should have DAC enabled" {
-            (Get-DbaSpConfigure -SqlInstance $psitem -ConfigName 'RemoteDACConnectionsEnabled').ConfiguredValue | Should Be 1
+        Context "Testing $_" {
+            It "$psitem Should have DAC enabled $DAC" {
+                (Get-DbaSpConfigure -SqlInstance $psitem -ConfigName 'RemoteDACConnectionsEnabled').ConfiguredValue -eq 1  | Should Be $DAC
+            }
         }
     }
 }
