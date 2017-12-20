6efa949d7d668d195c568edfe79e31c020e01760
Rob Sewell <mrrobsewell@outlook.com>
2017-12-20 18:42:34 +0000

    Added COntext for Instances

--- checks/goodcheckdb.Tests.ps1
+++ checks/goodcheckdb.Tests.ps1
@@ -3,19 +3,20 @@ $maxdays = Get-DbcConfigValue policy.integritycheckmaxdays
 $datapurity = Get-DbcConfigValue skip.datapuritycheck
 
 Describe 'Testing last good DBCC CHECKDB' -Tags Database, Corruption, Integrity, DBCC, $filename {
-	(Get-SqlInstance).ForEach{
-		$results = Get-DbaLastGoodCheckDb -SqlInstance $psitem
-		foreach ($result in $results) {
-			if ($result.Database -ne 'tempdb') {
-				It "last good integrity check for $($result.Database) on $psitem should be less than $maxdays" {
-					$result.LastGoodCheckDb | Should BeGreaterThan (Get-Date).AddDays(- ($maxdays))
-				}
+    (Get-SqlInstance).ForEach{
+        Context "Testing $_ " {
+            $results = Get-DbaLastGoodCheckDb -SqlInstance $psitem
+            foreach ($result in $results) {
+                if ($result.Database -ne 'tempdb') {
+                    It "last good integrity check for $($result.Database) on $psitem should be less than $maxdays" {
+                        $result.LastGoodCheckDb | Should BeGreaterThan (Get-Date).AddDays( - ($maxdays))
+                    }
 				
-				It -Skip:$datapurity "last good integrity check for $($result.Database) on $psitem has Data Purity Enabled" {
-					$result.DataPurityEnabled | Should Be $true
-				}
-				
-			}
-		}
-	}
+                    It -Skip:$datapurity "last good integrity check for $($result.Database) on $psitem has Data Purity Enabled" {
+                        $result.DataPurityEnabled | Should Be $true
+                    }
+                }
+            }
+        }
+    }
 }
