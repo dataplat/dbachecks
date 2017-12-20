f7d6a12b544cbcf9322e9eccc4d1daf6ece237ff
Rob Sewell <mrrobsewell@outlook.com>
2017-12-20 21:33:01 +0000

    removed connection tests and mergedinto Server Tests

--- checks/Server.Tests.ps1
+++ checks/Server.Tests.ps1
@@ -8,3 +8,28 @@ Describe 'Testing Server PowerPlan Configuration' -Tag PowerPlan, Server, $filen
         }
     }
 }
+
+$skipremote = Get-DbcConfigValue skip.remotingcheck
+$authscheme = Get-DbcConfigValue policy.authscheme
+
+Describe "Testing Instance Connectionn" -Tag Instance, Connection {
+    (Get-ComputerName).ForEach{
+        Context "$psitem Connection Tests" {
+            BeforeAll {
+                $Connection = Test-DbaConnection -SqlInstance $psitem 
+            }
+            It "$psitem Connects successfully" {
+                $Connection.connectsuccess | Should BE $true
+            }
+            It "$psitem Auth Scheme should be $authscheme" {
+                $connection.AuthScheme | Should Be $authscheme
+            }
+            It "$psitem Is pingable" {
+                $Connection.IsPingable | Should be $true
+            }
+            It -Skip:$skipremote "$psitem Is PSRemotebale" {
+                $Connection.PSRemotingAccessible | Should Be $True
+            }
+        }
+    }
+}
