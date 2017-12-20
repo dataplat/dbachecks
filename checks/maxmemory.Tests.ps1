ab6a6e09cd7361a4f983011326a53f2ca215e5d9
Cláudio Silva <claudiosil100@gmail.com>
2017-12-20 17:29:16 +0000

    Add missing call

--- checks/maxmemory.Tests.ps1
+++ checks/maxmemory.Tests.ps1
@@ -1,6 +1,7 @@
 $filename = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Describe 'Testing MaxMemory' -Tags Memory, $filename {
 	(Get-SqlInstance).ForEach{
+        $results = Get-DbaMaxMemory -SqlInstance $psitem
 		foreach ($result in $results) {
 			It "$psitem instance MaxMemory value $($result.SqlMaxMb) should be less than host total memory $($result.TotalMB)" {
 				$result.SqlMaxMb | Should BeLessThan $result.TotalMB
