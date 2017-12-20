81a401fe0f52ae4670137b960c199463215070e7
Rob Sewell <mrrobsewell@outlook.com>
2017-12-20 12:17:18 +0000

    added dbaowners should and should not tests

---
+++ checks/dbownershould.Tests.ps1
@@ -0,0 +1,16 @@
+﻿$filename = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+$skip = Get-DbcConfigValue skip.dbownershould
+$targetowner = Get-DbcConfigValue policy.dbownershould
+Describe 'Testing Database Owners' -Tags Database, Owner, $filename {
+    $PSDefaultParameterValues = @{ 'It:Skip' = $skip}
+    (Get-SqlInstance).ForEach{
+        Context "Testing $_ for Database Owners" {
+            $Results = Test-DbaDatabaseOwner -SqlInstance $_ -TargetLogin $targetowner
+            $Results.ForEach{
+                It "$($_.Database) Owner should be $targetowner" {
+                    $_.CurrentOwner | Should Be $_.TargetOwner
+                }
+            }
+        }
+    }
+}
---
+++ checks/dbownershouldnot.Tests.ps1
@@ -0,0 +1,16 @@
+﻿$filename = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+$skip = Get-DbcConfigValue skip.dbownershouldnot
+$targetowner = Get-DbcConfigValue policy.dbownershouldnot
+Describe 'Testing Database Owners' -Tags Database, Owner, $filename {
+    $PSDefaultParameterValues = @{ 'It:Skip' = $skip}
+    (Get-SqlInstance).ForEach{
+        Context "Testing $_ for Database Owners" {
+            $Results = Test-DbaDatabaseOwner -SqlInstance $_ -TargetLogin $targetowner
+            $Results.ForEach{
+                It "$($_.Database) Owner should Not be $targetowner" {
+                    $_.CurrentOwner | Should Not Be $_.TargetOwner
+                }
+            }
+        }
+    }
+}
