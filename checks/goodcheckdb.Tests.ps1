0aa10262891af0c06d1c93b00377761f7a41cb96
Rob Sewell <mrrobsewell@outlook.com>
2017-12-20 08:07:07 +0000

    Added DBCC to tag and moved the comparison to the right so that it shows in the results

--- checks/goodcheckdb.Tests.ps1
+++ checks/goodcheckdb.Tests.ps1
@@ -2,13 +2,13 @@
 $maxdays = Get-DbcConfigValue policy.integritycheckmaxdays
 $datapurity = Get-DbcConfigValue skip.datapuritycheck
 
-Describe 'Testing last good DBCC CHECKDB' -Tags Database, Corruption, Integrity, $filename {
+Describe 'Testing last good DBCC CHECKDB' -Tags Database, Corruption, Integrity, DBCC, $filename {
 	(Get-SqlInstance).ForEach{
 		$results = Get-DbaLastGoodCheckDb -SqlInstance $psitem
 		foreach ($result in $results) {
 			if ($result.Database -ne 'tempdb') {
 				It "last good integrity check for $($result.Database) on $psitem should be less than $maxdays" {
-					$result.LastGoodCheckDb -ge (Get-Date).AddDays(- ($maxdays)) | Should be $true
+					$result.LastGoodCheckDb  | Should BeGreaterThan (Get-Date).AddDays(- ($maxdays))
 				}
 				
 				if (-not $datapurity) {
