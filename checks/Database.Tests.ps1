65f6f05de2ef5b715b32de1290587a4991287bb2
Rob Sewell <mrrobsewell@outlook.com>
2017-12-20 21:46:43 +0000

    added dbwonersshouldnot added to database

--- checks/Database.Tests.ps1
+++ checks/Database.Tests.ps1
@@ -44,3 +44,17 @@ Describe 'Testing Database Owners' -Tags Database, Owner, $filename {
         }
     }
 }
+
+$targetowner = Get-DbcConfigValue policy.dbownershouldnot
+Describe 'Testing Database Owners' -Tags Database, Owner, $filename {
+    (Get-SqlInstance).ForEach{
+        Context "Testing $psitem for Database Owners" {
+            $results = Test-DbaDatabaseOwner -SqlInstance $psitem -TargetLogin $targetowner
+            $results.ForEach{
+                It "$($psitem.Database) Owner should Not be $targetowner" {
+                    $psitem.CurrentOwner | Should Not Be $psitem.TargetOwner
+                }
+            }
+        }
+    }
+}
