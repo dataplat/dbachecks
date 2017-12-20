35a0c7bc8ce10026a0b257abc91229bd4b1de618
Rob Sewell <mrrobsewell@outlook.com>
2017-12-20 11:55:04 +0000

    added connection tests

---
+++ checks/connection.Tests.ps1
@@ -0,0 +1,23 @@
+﻿$filename = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Describe "Testing Instance Connectionn" -Tag Instance, Connection {
+    (Get-ComputerName).ForEach{
+        Context "$_ Connection Tests" {
+            BeforeAll {
+                $Connection = Test-DbaConnection -SqlInstance $_ 
+            }
+            It "$_ Connects successfully" {
+                $Connection.connectsuccess | Should BE $true
+            }
+            It "$_ AUth Scheme should be NTLM" {
+                $connection.AuthScheme | Should Be "NTLM"
+            }
+            It "$_ Is pingable" {
+                $Connection.IsPingable | Should be $True
+            }
+            It "$_ Is PSRemotebale" {
+                $Connection.PSRemotingAccessible | Should Be $True
+            }
+        }
+    }
+
+}
