61cb10158cba647009b2a6d56474ff6e860e5f86
Rob Sewell <mrrobsewell@outlook.com>
2017-12-20 20:03:38 +0000

    Yup the comma is important

--- checks/Server.Tests.ps1
+++ checks/Server.Tests.ps1
@@ -1,5 +1,5 @@
 $filename = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
-Describe 'Testing Server PowerPlan Configuration' -Tag PowerPlan, Server $filename {
+Describe 'Testing Server PowerPlan Configuration' -Tag PowerPlan, Server, $filename {
     (Get-ComputerName).ForEach{
         Context "Testing $_" {
             It "Server PowerPlan should be High Performance" {
