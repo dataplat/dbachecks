a64ce1f6e6c743957578c6185094f7cf0bef8a81
potatoqualitee <clemaire@gmail.com>
2017-12-21 21:20:39 +0100

    udpates

--- checks/Database.Tests.ps1
+++ checks/Database.Tests.ps1
@@ -17,7 +17,7 @@ Describe "Last Backup Restore & Integrity Checks" -Tag Backup, BackupTest, $file
 		$destdata = Get-DbcConfigValue setup.backupdatadir
 		$destlog = Get-DbcConfigValue setup.backuplogdir
 		(Get-SqlInstance).ForEach{
-			foreach ($result in (Test-DbaLastBackup -SqlInstance $psitem -Destination $destserver -LogDirectory $destlog -DataDirectory $destdata )) {
+			foreach ($result in (Test-DbaLastBackup -SqlInstance $psitem -Destination $destserver -LogDirectory $destlog -DataDirectory $destdata)) {
 				if ($result.DBCCResult -notmatch 'skipped for restored master') {
 					It "DBCC for $($result.Database) on $psitem should be success" {
 						$result.DBCCResult | Should Be 'Success'
@@ -34,51 +34,51 @@ Describe "Last Backup Restore & Integrity Checks" -Tag Backup, BackupTest, $file
 
 Describe "Database Owners" -Tag DatabaseOwner, $filename {
 	$targetowner = Get-DbcConfigValue policy.dbownershould
-    (Get-SqlInstance).ForEach{
-        Context "Testing $psitem for Database Owners" {
-            $results = Test-DbaDatabaseOwner -SqlInstance $psitem -TargetLogin $targetowner
-            $results.ForEach{
-                It "$($psitem.Database) Owner should be $targetowner" {
-                    $psitem.CurrentOwner | Should Be $psitem.TargetOwner
-                }
-            }
-        }
-    }
+	(Get-SqlInstance).ForEach{
+		Context "Testing $psitem for Database Owners" {
+			$results = Test-DbaDatabaseOwner -SqlInstance $psitem -TargetLogin $targetowner
+			$results.ForEach{
+				It "$($psitem.Database) Owner should be $targetowner" {
+					$psitem.CurrentOwner | Should Be $psitem.TargetOwner
+				}
+			}
+		}
+	}
 }
 
 Describe "Database Owners" -Tag NotDatabaseOwner, $filename {
 	$targetowner = Get-DbcConfigValue policy.dbownershouldnot
-    (Get-SqlInstance).ForEach{
-        Context "Testing $psitem for Database Owners" {
-            $results = Test-DbaDatabaseOwner -SqlInstance $psitem -TargetLogin $targetowner
-            $results.ForEach{
-                It "$($psitem.Database) Owner should Not be $targetowner" {
-                    $psitem.CurrentOwner | Should Not Be $psitem.TargetOwner
-                }
-            }
-        }
-    }
+	(Get-SqlInstance).ForEach{
+		Context "Testing $psitem for Database Owners" {
+			$results = Test-DbaDatabaseOwner -SqlInstance $psitem -TargetLogin $targetowner
+			$results.ForEach{
+				It "$($psitem.Database) Owner should Not be $targetowner" {
+					$psitem.CurrentOwner | Should Not Be $psitem.TargetOwner
+				}
+			}
+		}
+	}
 }
 
 Describe "last good DBCC CHECKDB" -Tag Corruption, Integrity, DBCC, $filename {
 	$maxdays = Get-DbcConfigValue policy.integritycheckmaxdays
 	$datapurity = Get-DbcConfigValue skip.datapuritycheck
-    (Get-SqlInstance).ForEach{
-        Context "Testing $psitem " {
-            $results = Get-DbaLastGoodCheckDb -SqlInstance $psitem
-            foreach ($result in $results) {
-                if ($result.Database -ne 'tempdb') {
-                    It "last good integrity check for $($result.Database) on $psitem should be less than $maxdays" {
-                        $result.LastGoodCheckDb | Should BeGreaterThan (Get-Date).AddDays(-($maxdays))
-                    }
-				
-                    It -Skip:$datapurity "last good integrity check for $($result.Database) on $psitem has Data Purity Enabled" {
-                        $result.DataPurityEnabled | Should Be $true
-                    }
-                }
-            }
-        }
-    }
+	(Get-SqlInstance).ForEach{
+		Context "Testing $psitem " {
+			$results = Get-DbaLastGoodCheckDb -SqlInstance $psitem
+			foreach ($result in $results) {
+				if ($result.Database -ne 'tempdb') {
+					It "last good integrity check for $($result.Database) on $psitem should be less than $maxdays" {
+						$result.LastGoodCheckDb | Should BeGreaterThan (Get-Date).AddDays(-($maxdays))
+					}
+					
+					It -Skip:$datapurity "last good integrity check for $($result.Database) on $psitem has Data Purity Enabled" {
+						$result.DataPurityEnabled | Should Be $true
+					}
+				}
+			}
+		}
+	}
 }
 
 Describe "Column Identity Usage" -Tag Identity, $filename {
@@ -122,19 +122,19 @@ Describe "Last Backup Times" -Tag Backup, DISA, LastBackup, $filename {
 		Context "Testing backups on $psitem" {
 			$results = Get-DbaDatabase -SqlInstance $psitem
 			foreach ($result in $results) {
-				if ($result.RecoveryModel -ne 'Simple') {
-					It "full backups for $result should be less than $maxfull days" {
-						$result.LastFullBackup -ge (Get-Date).AddDays(- ($maxfull)) | Should be $true
-					}
+				
+				It "full backups for $result should be less than $maxfull days" {
+					$result.LastFullBackup -ge (Get-Date).AddDays(-($maxfull)) | Should be $true
 				}
 				
 				It -Skip:$diffskip "diff backups for $result should be less than $maxdiff hours" {
-					$result.LastDiffBackup -ge (Get-Date).AddHours(- ($maxdiff)) | Should be $true
+					$result.LastDiffBackup -ge (Get-Date).AddHours(-($maxdiff)) | Should be $true
 				}
 				
-				
-				It "log backups for $result should be less than $maxlog minutes" {
-					$result.LastLogBackup -ge (Get-Date).AddMinutes(- ($maxlog)) | Should be $true
+				if ($result.RecoveryModel -ne 'Simple') {
+					It "log backups for $result should be less than $maxlog minutes" {
+						$result.LastLogBackup -ge (Get-Date).AddMinutes(-($maxlog)) | Should be $true
+					}
 				}
 			}
 		}
