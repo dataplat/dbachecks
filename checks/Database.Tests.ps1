35eba3ca69b50541778c877885f563a8d464bf7a
potatoqualitee <clemaire@gmail.com>
2017-12-21 17:47:24 +0100

    added minus closer to var and added back last backup times

--- checks/Database.Tests.ps1
+++ checks/Database.Tests.ps1
@@ -69,7 +69,7 @@ Describe 'Testing last good DBCC CHECKDB' -Tags Corruption, Integrity, DBCC, $fi
             foreach ($result in $results) {
                 if ($result.Database -ne 'tempdb') {
                     It "last good integrity check for $($result.Database) on $psitem should be less than $maxdays" {
-                        $result.LastGoodCheckDb | Should BeGreaterThan (Get-Date).AddDays( - ($maxdays))
+                        $result.LastGoodCheckDb | Should BeGreaterThan (Get-Date).AddDays(-($maxdays))
                     }
 				
                     It -Skip:$datapurity "last good integrity check for $($result.Database) on $psitem has Data Purity Enabled" {
@@ -112,3 +112,31 @@ Describe 'Testing Full Recovery Model' -Tags Database, DISA, RecoveryModel, $fil
 	}
 }
 
+Describe 'Testing Last Backup Times' -Tags Backup, DISA, LastBackup, $filename {
+	(Get-SqlInstance).ForEach{
+		$maxfull = Get-DbcConfigValue policy.backupfullmaxdays
+		$maxdiff = Get-DbcConfigValue policy.backupdiffmaxhours
+		$maxlog = Get-DbcConfigValue policy.backuplogmaxminutes
+		$diffskip = Get-DbcConfigValue skip.backupdiffcheck
+		
+		Context "Testing backups on $psitem" {
+			$results = Get-DbaDatabase -SqlInstance $psitem
+			foreach ($result in $results) {
+				if ($result.RecoveryModel -ne 'Simple') {
+					It "full backups for $result should be less than $maxfull days" {
+						$result.LastFullBackup -ge (Get-Date).AddDays(- ($maxfull)) | Should be $true
+					}
+				}
+				
+				It -Skip:$diffskip "diff backups for $result should be less than $maxdiff hours" {
+					$result.LastDiffBackup -ge (Get-Date).AddHours(- ($maxdiff)) | Should be $true
+				}
+				
+				
+				It "log backups for $result should be less than $maxlog minutes" {
+					$result.LastLogBackup -ge (Get-Date).AddMinutes(- ($maxlog)) | Should be $true
+				}
+			}
+		}
+	}
+}
