1e914305c8e65c214956f12e4d807ffb5ab5cf5e
potatoqualitee <clemaire@gmail.com>
2017-12-21 19:23:16 +0100

    tons of changes

--- functions/Update-DbcPowerBiDataSource.ps1
+++ functions/Update-DbcPowerBiDataSource.ps1
@@ -1,56 +1,87 @@
 ﻿function Update-DbcPowerBiDataSource {
 	<#
 		.SYNOPSIS
-		Runs default tests and exports file in required format for launching the PowerBi command
+		Converts Pester results and exports file in required format for launching the PowerBi command
 		
 		.DESCRIPTION
-		Runs default tests and exports file in required format for launching the PowerBi command
+		Converts Pester results and exports file in required format for launching the PowerBi command
 	
 		Basically does this:
-			$InputObject = Invoke-DbcCheck -Show Summary -PassThru
-			$InputObject.TestResult | Select-Object -First 20 | ConvertTo-Json -Depth 5 | Out-File "$env:windir\temp\dbachecks.json"
+			$InputObject.TestResult | Select-Object -First 20 | ConvertTo-Json -Depth 3 | Out-File "$env:windir\temp\dbachecks.json"
 
 		.PARAMETER InputObject
-		Resultset from Invoke-DbcCheck. If InputObject is not provided, it will be generated using a very generic resultset:
+		Required. Resultset from Invoke-DbcCheck. If InputObject is not provided, it will be generated using a very generic resultset:
 	
 		Invoke-DbcCheck -Show Summary -PassThru
 	
 		.PARAMETER Path
-		Where to store your JSON files. "$env:windir\temp\dbachecks.json" by default
+		The directory to store your JSON files. "C:\windows\temp\dbachecks\*.json" by default
 	
-		.EXAMPLE
-		PS C:\> Update-DbcPowerBiDataSource
-		
-		Runs default tests, saves to json to "$env:windir\temp\dbachecks.json"
+		PARAMETER Environment
+		Tag your JSON filename with an enviornment
 	
 		.EXAMPLE
 		PS C:\> Invoke-DbcCheck -SqlInstance sql2017 -Tag identity -Show Summary -PassThru | Update-DbcPowerBiDataSource
 		
-		Runs backup tests against sql2017 then saves to json to "$env:windir\temp\dbachecks.json"
+		Runs backup tests against sql2017 then saves to json to "$env:windir\temp\dbachecks\dbachecks_identity.json"
+	
+		.EXAMPLE
+		PS C:\> Invoke-DbcCheck -SqlInstance sql2017 -Tag identity -Show Summary -PassThru | Update-DbcPowerBiDataSource
+		
+		Runs backup tests against sql2017 then saves to json to "$env:windir\temp\dbachecks\dbachecks_identity.json"
 
 		.EXAMPLE
 		PS C:\> Invoke-DbcCheck -SqlInstance sql2017 -Tag Backup -Show Summary -PassThru | Update-DbcPowerBiDataSource -Path \\nas\projects\dbachecks.json
 		PS C:\> Start-DbcPowerBi -Path \\nas\projects\dbachecks.json
 		
-		Runs tests, saves to json to \\nas\projects\dbachecks.json
+		Runs tests, saves to json to \\nas\projects\dbachecks.json but then you'll have to change your data source in Power BI because by default it points to C:\Windows\Temp (limitation of Power BI)
     #>
 	[CmdletBinding()]
 	param (
-		[parameter(ValueFromPipeline)]
-		[object]$InputObject,
-		[string]$Path = "$env:windir\temp\dbachecks.json"
+		[parameter(ValueFromPipeline, Mandatory)]
+		[pscustomobject]$InputObject,
+		[string]$Path = "$env:windir\temp\dbachecks",
+		[string]$Enviornment
 	)
 	process {
-		
-		
 		try {
-			if (-not $InputObject) {
-				$InputObject = Invoke-DbcCheck -Show Summary -PassThru
+			if (-not (Test-Path -Path $Path)) {
+				New-Item -ItemType Directory -Path $Path -ErrorAction Stop
 			}
-			$InputObject.TestResult | ConvertTo-Json -Depth 3 | Out-File -FilePath $Path
 		}
 		catch {
 			Stop-PSFFunction -Message "Failure" -Exception $_
+			return
+		}
+		
+		if (-not $InputObject.TestResult) {
+			Stop-PSFFunction -Message "InputObject does not contain a TestResult"
+			return
+		}
+		
+		$basename = "dbachecks"
+		if ($InputObject.TagFilter) {
+			$basename = "$basename`_$($InputObject.TagFilter -join "_")"
+		}
+		
+		if ($Enviornment) {
+			$basename = "$basename`_$Enviornment"
+		}
+		
+		$filename = "$Path\$basename.json"
+		
+		try {
+			$InputObject.TestResult | ConvertTo-Json -Depth 3 | Out-File -FilePath $filename
+			Write-PSFMessage -Level Output -Message "Wrote results to $filename"
+		}
+		catch {
+			Stop-PSFFunction -Message "Failure" -Exception $_
+			return
+		}
+	}
+	end {
+		if (-not $InputObject) {
+			Stop-PSFFunction -Message "InputObject is null. Did you forget to specify -Passthru for your previous command?"
 			return
 		}
 	}
