b7b56ceedcc815f89f42b766e520a19d4f4f645f
potatoqualitee <clemaire@gmail.com>
2017-12-22 11:00:48 +0100

    command for real

---
+++ functions/Get-DbcCheck.ps1
@@ -0,0 +1,31 @@
+﻿function Get-DbcCheck {
+	<#
+		.SYNOPSIS
+			Lists all checks, tags and unique identifiers
+	
+		.DESCRIPTION
+			
+		.PARAMETER Name
+			Default: "*"
+			The name of the check to retrieve.
+			May be any string, supports wildcards.
+
+		.EXAMPLE
+			PS C:\> Get-DbcCheck
+			
+			Retrieves all of the available checks
+
+		.EXAMPLE
+			PS C:\> Get-DbcCheck backups
+			
+			Retrieves all of the available tags that match backups
+    #>
+	[CmdletBinding()]
+	param (
+		[string]$Name = "*"
+	)
+	
+	process {
+		Get-Content "$script:localapp\checks.json" | ConvertFrom-Json
+	}
+}
