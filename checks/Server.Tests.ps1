a431e182181d6c80a4aa7018d5194e04fc454428
potatoqualitee <clemaire@gmail.com>
2017-12-22 20:43:46 +0100

    server psitem updates

--- checks/Server.Tests.ps1
+++ checks/Server.Tests.ps1
@@ -1,62 +1,60 @@
 $filename = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 
 Describe "Server Power Plan Configuration" -Tags PowerPlan, $filename {
-    (Get-ComputerName).ForEach{
-        Context "Testing Server Power Plan Configuration on $psitem" {
-            It "Server PowerPlan should be High Performance" {
-                (Test-DbaPowerPlan -ComputerName $_).IsBestPractice | Should be $true
-            }
-        }
-    }
+	(Get-ComputerName).ForEach{
+		Context "Testing Server Power Plan Configuration on $psitem" {
+			It "PowerPlan is High Performance" {
+				(Test-DbaPowerPlan -ComputerName $psitem).IsBestPractice | Should be $true
+			}
+		}
+	}
 }
 
 Describe "Instance Connection" -Tags InstanceConnection, Connectivity, $filename {
-    $skipremote = Get-DbcConfigValue skip.remotingcheck
-    $authscheme = Get-DbcConfigValue policy.authscheme
-    (Get-ComputerName).ForEach{
-        Context "Testing Instance Connection on $psitem" {
-            BeforeAll {
-                $Connection = Test-DbaConnection -SqlInstance $psitem
-            }
-            It "$psitem Connects successfully" {
-                $Connection.connectsuccess | Should BE $true
-            }
-            It "$psitem Auth Scheme should be $authscheme" {
-                $connection.AuthScheme | Should Be $authscheme
-            }
-            It "$psitem Is pingable" {
-                $Connection.IsPingable | Should be $true
-            }
-            It -Skip:$skipremote "$psitem Is PSRemotebale" {
-                $Connection.PSRemotingAccessible | Should Be $True
-            }
-        }
-    }
+	$skipremote = Get-DbcConfigValue skip.remotingcheck
+	$authscheme = Get-DbcConfigValue policy.authscheme
+	(Get-ComputerName).ForEach{
+		Context "Testing Instance Connection on $psitem" {
+			$connection = Test-DbaConnection -SqlInstance $psitem
+			It "$psitem Connects successfully" {
+				$connection.connectsuccess | Should BE $true
+			}
+			It "$psitem Auth Scheme should be $authscheme" {
+				$connection.AuthScheme | Should Be $authscheme
+			}
+			It "$psitem Is pingable" {
+				$connection.IsPingable | Should be $true
+			}
+			It -Skip:$skipremote "$psitem Is PSRemotebale" {
+				$Connection.PSRemotingAccessible | Should Be $True
+			}
+		}
+	}
 }
 
 Describe "SPNs" -Tags SPN, $filename {
-    (Get-ComputerName).ForEach{
-        Context "Testing SPNs on $psitem" {
-            $results = Test-DbaSpn -ComputerName $psitem
-            foreach ($result in $results) {
-                It "$psitem should have SPN for $($result.RequiredSPN) for $($result.InstanceServiceAccount)" {
-                    $result.Error | Should Be 'None'
-                }
-            }
-        }
-    }
+	(Get-ComputerName).ForEach{
+		Context "Testing SPNs on $psitem" {
+			$computer = $psitem
+			(Test-DbaSpn -ComputerName $psitem).ForEach{
+				It "$computer should have SPN for $($psitem.RequiredSPN) for $($psitem.InstanceServiceAccount)" {
+					$psitem.Error | Should Be 'None'
+				}
+			}
+		}
+	}
 }
 
 Describe "Disk Space" -Tags DiskCapacity, Storage, DISA, $filename {
-    $free = Get-DbcConfigValue policy.diskspacepercentfree
-    (Get-ComputerName).ForEach{
-        Context "Testing Disk Space on $psitem" {
-            $results = Get-DbaDiskSpace -ComputerName $psitem
-            foreach ($result in $results) {
-                It "$($result.Name) on $psitem should be at least $free percent free" {
-                    $result.PercentFree -ge $free | Should be $true
-                }
-            }
-        }
-    }
+	$free = Get-DbcConfigValue policy.diskspacepercentfree
+	(Get-ComputerName).ForEach{
+		Context "Testing Disk Space on $psitem" {
+			$computer = $psitem
+			(Get-DbaDiskSpace -ComputerName $psitem).ForEach{
+				It "$($psitem.Name) on $computer should be at least $free percent free" {
+					$psitem.PercentFree -ge $free | Should be $true
+				}
+			}
+		}
+	}
 }
