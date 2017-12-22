43854c6f218ffd3c41c01ecb65182049acf96256
Shawn Melton <wshawnmelton@gmail.com>
2017-12-22 08:38:10 -0600

    minor adjustments to format

--- functions/Set-DbcConfig.ps1
+++ functions/Set-DbcConfig.ps1
@@ -1,39 +1,42 @@
 ﻿function Set-DbcConfig {
-<#
-	.SYNOPSIS
-		Sets configuration entries.
+	<#
+		.SYNOPSIS
+			Sets configuration entries.
 	
-	.DESCRIPTION
-		This function creates or changes configuration values.
-		These can be used to provide dynamic configuration information outside the PowerShell variable system.
-	
-	.PARAMETER Name
-		Name of the configuration entry.
-	
-	.PARAMETER Value
-		The value to assign to the named configuration element.
-	
-	.PARAMETER Handler
-		A scriptblock that is executed when a value is being set.
-		Is only executed if the validation was successful (assuming there was a validation, of course)
-	
-	.PARAMETER EnableException
-		Replaces user friendly yellow warnings with bloody red exceptions of doom!
-		Use this if you want the function to throw terminating errors you want to catch.
+		.DESCRIPTION
+			This function creates or changes configuration values.
 
-	.EXAMPLE
-		PS C:\> Set-DbcConfig -Name Lists.SqlServers -Value sql2016, sql2017, sqlcluster
-		
-		Resets the lists.sqlservers entry to sql2016, sql2017, sqlcluster
+			These can be used to provide dynamic configuration information outside the PowerShell variable system.
 	
-	.EXAMPLE
-		PS C:\> Set-DbcConfig -Name Lists.SqlServers -Value sql2016, sql2017, sqlcluster -Append
+		.PARAMETER Name
+			Name of the configuration entry.
+	
+		.PARAMETER Value
+			The value to assign to the named configuration element.
+	
+		.PARAMETER Handler
+			A scriptblock that is executed when a value is being set.
+
+			Is only executed if the validation was successful (assuming there was a validation, of course)
+	
+		.PARAMETER EnableException
+			Replaces user friendly yellow warnings with bloody red exceptions of doom!
+
+			Use this if you want the function to throw terminating errors you want to catch.
+
+		.EXAMPLE
+			PS C:\> Set-DbcConfig -Name Lists.SqlServers -Value sql2016, sql2017, sqlcluster
 		
-		Addds on to the current lists.sqlservers entry with sql2016, sql2017, sqlcluster
-#>
+			Resets the lists.sqlservers entry to sql2016, sql2017, sqlcluster
+	
+		.EXAMPLE
+			PS C:\> Set-DbcConfig -Name Lists.SqlServers -Value sql2016, sql2017, sqlcluster -Append
+		
+			Addds on to the current lists.sqlservers entry with sql2016, sql2017, sqlcluster
+	#>
 	[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseShouldProcessForStateChangingFunctions", "")]
 	[CmdletBinding(DefaultParameterSetName = "FullName")]
-	Param (
+	param (
 		[string]$Name,
 		[AllowNull()]
 		[AllowEmptyCollection()]
@@ -43,6 +46,7 @@
 		[switch]$Append,
 		[switch]$EnableException
 	)
+
 	process {
 		if (-not (Get-DbcConfig -Name $Name)) {
 			Stop-PSFFunction -Message "Setting named $Name does not exist. If you'd like us to support an additional setting, please file a GitHub issue."
