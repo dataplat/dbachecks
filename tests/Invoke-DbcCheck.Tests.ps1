799f3d99fe84b007d389be45cd067dc16e2cc37b
potatoqualitee <clemaire@gmail.com>
2017-12-23 20:56:34 +0100

    had to skip because it returns the instance object thing again
    issue with Pester I think, @SQLDBAWithABeard

---
+++ tests/Invoke-DbcCheck.Tests.ps1
@@ -0,0 +1,12 @@
+﻿$commandname = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot\constants.ps1"
+
+Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
+	Context "Command executes properly and returns proper info" {
+		It -Skip "runs a check" {
+			$results = Invoke-DbcCheck -ComputerName localhost -Tag DiskCapacity -Passthru
+			$results.TestResult | Should Not Be $null # Because nothing else works right now
+		}
+	}
+}
