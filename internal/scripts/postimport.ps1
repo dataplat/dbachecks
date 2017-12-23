16978b4c129ecb4b550e102c6a26372e9914da4a
potatoqualitee <clemaire@gmail.com>
2017-12-23 17:58:25 +0100

    cleaned up tags

--- internal/scripts/postimport.ps1
+++ internal/scripts/postimport.ps1
@@ -7,7 +7,7 @@ foreach ($file in (Get-ChildItem "$ModuleRoot\internal\configurations\*.ps1")) {
 
 # Parse repo for tags and descriptions then write json
 $repos = Get-CheckRepo
-$collection = $repofiles = @()
+$collection = $groups = $repofiles = @()
 foreach ($repo in $repos) {
 	$repofiles += Get-ChildItem "$repo\*.Tests.ps1"
 }
@@ -18,6 +18,7 @@ foreach ($file in $repofiles) {
 	foreach ($string in $strings) {
 		$describe = Select-String -InputObject $string -Pattern 'Describe\ \"[\s]*(.+)[\s]*\"\ \-Tags' | ForEach-Object { $_.matches.Groups[1].Value }
 		$tags = Select-String -InputObject $string -Pattern '-Tags[\s]*(.+)[\s]*\, \$filename' | ForEach-Object { $_.matches.Groups[1].Value }
+		$groups += $filename
 		$collection += [pscustomobject]@{
 			Group			    = $filename
 			Description		    = $describe
@@ -27,7 +28,7 @@ foreach ($file in $repofiles) {
 	}
 }
 
-$singletags = ($collection.AllTags -split ",").Trim() | Group-Object | Where-Object Count -eq 1
+$singletags = ($collection.AllTags -split ",").Trim() | Group-Object | Where-Object { $_.Count -eq 1 -and $_.Name -notin $groups }
 foreach ($check in $collection) {
 	$unique = $singletags | Where-Object { $_.Name -in ($check.AllTags -split ",").Trim() }
 	$check.UniqueTag = $unique.Name
