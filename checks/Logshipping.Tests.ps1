89b8f2a79892b05e8e7de538f3e6a1b20a0689a0
Sander Stad <sander@sqlstad.nl>
2018-01-02 15:28:53 +0100

    Removed commented code

--- checks/Logshipping.Tests.ps1
+++ checks/Logshipping.Tests.ps1
@@ -7,8 +7,6 @@ Describe "Log Shipping State" -Tags Logshipping, $filename {
 		Context "Testing the primary databases on $psitem" {
 			@(Test-DbaLogShippingStatus -SqlInstance $psitem -Primary).ForEach{
 				It "Status should be OK for $($psitem.Database) on $($psitem.SqlInstance)" {
-					#$timeDifference = (Get-Date) - $psitem.TimeSinceLastBackup
-					#$timeDifference.Minutes | Should BeLessThan $psitem.BackupThresshold
 					$psitem.Status | Should Be "All OK"
 				}
 			}
@@ -19,14 +17,6 @@ Describe "Log Shipping State" -Tags Logshipping, $filename {
 				It "Status should be OK for $($psitem.Database) on $($psitem.SqlInstance)" {
 					$psitem.Status | Should Be "All OK"
 				}
-				<# It "Time since last restore should be less than ($($psitem.RestoreThresshold))" {
-					$timeDifference = (Get-Date) - $psitem.TimeSinceLastRestore
-					$timeDifference.Minutes | Should BeLessThan $psitem.RestoreThresshold
-				}
-
-				It "Restore latency should be less than ($($psitem.RestoreThresshold))" {
-					$psitem.LastRestoredLatency | Should BeLessThan $psitem.RestoreThresshold
-				} #>
 			}
 		}
 
