0180bb354af552036d1a3e6fdaaa92f397313159
Sander Stad <sander@sqlstad.nl>
2018-01-02 17:41:15 +0100

    Changed the tags for the tests

--- checks/LogShipping.Tests.ps1
+++ checks/LogShipping.Tests.ps1
@@ -1,9 +1,6 @@
 $filename = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
-
-
-Describe "Log Shipping State" -Tags Logshipping, $filename {
+Describe "Log Shipping Status Primary" -Tags LogShippingPrimary, $filename {
 	(Get-SqlInstance).ForEach{
-
 		Context "Testing the primary databases on $psitem" {
 			@(Test-DbaLogShippingStatus -SqlInstance $psitem -Primary).ForEach{
 				It "Status should be OK for $($psitem.Database) on $($psitem.SqlInstance)" {
@@ -11,7 +8,10 @@ Describe "Log Shipping State" -Tags Logshipping, $filename {
 				}
 			}
 		}
-
+	}
+}
+Describe "Log Shipping Status Primary" -Tags LogShippingSecondary, $filename {
+	(Get-SqlInstance).ForEach{
 		Context "Testing the secondary databases on $psitem" {
 			@(Test-DbaLogShippingStatus -SqlInstance $psitem -Secondary).ForEach{
 				It "Status should be OK for $($psitem.Database) on $($psitem.SqlInstance)" {
@@ -19,8 +19,5 @@ Describe "Log Shipping State" -Tags Logshipping, $filename {
 				}
 			}
 		}
-
-	} # end for each instance
-
-} # end describe
-
+	}
+}
