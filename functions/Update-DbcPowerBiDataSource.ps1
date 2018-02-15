f1d17d784e03ccacf858979c9e075dbfc1d910a5
Rob Sewell <mrrobsewell@outlook.com>
2018-02-15 15:20:53 +0000

    added help to say that you need to refresh

--- functions/Update-DbcPowerBiDataSource.ps1
+++ functions/Update-DbcPowerBiDataSource.ps1
@@ -2,10 +2,11 @@
     <#
         .SYNOPSIS
             Converts Pester results and exports file in required format for launching the PowerBi command
+            YOU WILL NEED TO REFRESH the PowerBi TO SEE THE NEW Results
 
         .DESCRIPTION
             Converts Pester results and exports file in required format for launching the PowerBi command
-
+            YOU WILL NEED TO REFRESH the PowerBi TO SEE THE NEW Results
             Basically does this:
                 $InputObject.TestResult | Select-Object -First 20 | ConvertTo-Json -Depth 3 | Out-File "$env:windir\temp\dbachecks.json"
 
@@ -82,8 +83,8 @@
             $basename = "$basename`_$($InputObject.TagFilter -join "_")"
         }
         
-        if ($Enviornment) {
-            $basename = "$basename`_$Enviornment"
+        if ($Environment) {
+            $basename = "$basename`_$Environment"
         }
         
         $filename = "$Path\$basename.json"
