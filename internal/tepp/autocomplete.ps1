27602acb63ed124f14118d8fdcbdd876346bcc5d
Rob Sewell <mrrobsewell@outlook.com>
2018-02-17 12:51:52 +0000

    I changed the name of the parameter

--- internal/tepp/autocomplete.ps1
+++ internal/tepp/autocomplete.ps1
@@ -1,14 +1,14 @@
-﻿# Register that script block
+# Register that script block
 Register-PSFTeppScriptblock -Name SqlInstance -ScriptBlock { Get-PSFConfig -Module dbachecks -Name SqlInstance }
 Register-PSFTeppScriptblock -Name ComputerName -ScriptBlock { Get-PSFConfig -Module dbachecks -Name ComputerName }
 Register-PSFTeppScriptblock -Name confignames -ScriptBlock { (Get-PSFConfig -Module dbachecks).Name }
 Register-PSFTeppScriptblock -Name tags -ScriptBlock { Get-DbcTagCollection }
-Register-PSFTeppScriptblock -Name enviornments -ScriptBlock { "Production", "Development", "Test" }
+Register-PSFTeppScriptblock -Name environments -ScriptBlock { "Production", "Development", "Test" }
 Register-PSFTeppScriptblock -Name policy.database.filegrowthtype  -ScriptBlock { "kb", "percent" }
 
 # Register the actual auto completer
-Register-PSFTeppArgumentCompleter -Command Update-DbcPowerBiDataSource -Parameter Enviornment -Name enviornments
-Register-PSFTeppArgumentCompleter -Command Clear-DbcPowerBiDataSource -Parameter Enviornment -Name enviornments
+Register-PSFTeppArgumentCompleter -Command Update-DbcPowerBiDataSource -Parameter Environment -Name environments
+Register-PSFTeppArgumentCompleter -Command Clear-DbcPowerBiDataSource -Parameter Environment -Name environments
 Register-PSFTeppArgumentCompleter -Command Invoke-DbcCheck -Parameter Check -Name tags
 Register-PSFTeppArgumentCompleter -Command Invoke-DbcCheck -Parameter ExcludeCheck  -Name tags
 Register-PSFTeppArgumentCompleter -Command Get-DbcConfig -Parameter Name -Name confignames
