359cb678ff34dc2e678e8cb4b096f4fe9fdc0102
Rob Sewell <mrrobsewell@outlook.com>
2018-02-17 12:52:39 +0000

    added auto complete for get-dbcconfigvalue

--- internal/tepp/autocomplete.ps1
+++ internal/tepp/autocomplete.ps1
@@ -1,4 +1,4 @@
-# Register that script block
+﻿# Register that script block
 Register-PSFTeppScriptblock -Name SqlInstance -ScriptBlock { Get-PSFConfig -Module dbachecks -Name SqlInstance }
 Register-PSFTeppScriptblock -Name ComputerName -ScriptBlock { Get-PSFConfig -Module dbachecks -Name ComputerName }
 Register-PSFTeppScriptblock -Name confignames -ScriptBlock { (Get-PSFConfig -Module dbachecks).Name }
@@ -12,6 +12,7 @@ Register-PSFTeppArgumentCompleter -Command Clear-DbcPowerBiDataSource -Parameter
 Register-PSFTeppArgumentCompleter -Command Invoke-DbcCheck -Parameter Check -Name tags
 Register-PSFTeppArgumentCompleter -Command Invoke-DbcCheck -Parameter ExcludeCheck  -Name tags
 Register-PSFTeppArgumentCompleter -Command Get-DbcConfig -Parameter Name -Name confignames
+Register-PSFTeppArgumentCompleter -Command Get-DbcConfigValue -Parameter Name -Name confignames
 Register-PSFTeppArgumentCompleter -Command Set-DbcConfig -Parameter Name -Name confignames
 Register-PSFTeppArgumentCompleter -Command Get-DbcTagCollection -Parameter Name -Name tags
 Register-PSFTeppArgumentCompleter -Command Set-DbcConfig -Parameter Value -Name policy.database.filegrowthtype 
