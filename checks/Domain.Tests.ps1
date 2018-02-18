640eea9bf099d26a34812c96c3d643ad17333caa
Rob Sewell <mrrobsewell@outlook.com>
2018-02-18 07:45:31 +0000

    Be Gone foul -

--- checks/Domain.Tests.ps1
+++ checks/Domain.Tests.ps1
@@ -4,7 +4,7 @@ Describe "Active Directory Domain Name" -Tags DomainName, $filename {
     $domain = Get-DbcConfigValue -Name domain.name
     (Get-ComputerName).ForEach{
         Context "Testing Active Directory Domain Name on $psitem" {
-            It "$psitem Should -Be on the Domain $domain" {
+            It "$psitem Should Be on the Domain $domain" {
                 (Get-DbaCmObject -Class Win32_ComputerSystem -ComputerName $psitem -Credential $credential).Domain | Should -Be $domain
             }
         }
@@ -20,7 +20,7 @@ Describe "Active Directory OU" -Tags OrganizationalUnit, $filename {
                 # Can be passed by Invoke-DbcCheck -Value
                 $value = Get-DbcConfigValue -Name domain.organizationalunit
             }
-            It -Skip "$psitem Should -Be in the right OU ($value)" {
+            It -Skip "$psitem Should Be in the right OU ($value)" {
                 (Get-ADComputer $psitem -Properties CanonicalName -Server $dc).CanonicalName | Should -Be $value
             }
         }
