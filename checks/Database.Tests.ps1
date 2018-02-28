4c85825574c43b38ee66526309f26d3eb4141ba1
tootblan <koglerk@csl.edu>
2018-02-27 20:08:08 -0600

    updated math to use raw bytes and fixed a logical flaw in the division

--- checks/Database.Tests.ps1
+++ checks/Database.Tests.ps1
@@ -346,8 +346,8 @@ Describe "Future File Growth" -Tags FutureFileGrowth, $filename {
         Context "Testing for files likely to grow soon on $psitem" {
             @(Get-DbaDatabase -SqlInstance $psitem -ExcludeDatabase $exclude | Select-Object SqlInstance, Name).ForEach{
                 $Files = Get-DbaDatabaseFile -SqlInstance $psitem.SqlInstance -Database $psitem.Name
-                $Files | Add-Member ScriptProperty -Name PercentFree -Value {[Math]::Round(([int64]$PSItem.AvailableSpace / [int64]$PSItem.Size) * 100, 3)}
-                $files | ForEach-Object {
+                $Files | Add-Member ScriptProperty -Name PercentFree -Value {100-[Math]::Round(([int64]$PSItem.UsedSpace.Byte / [int64]$PSItem.Size.Byte) * 100, 3)}
+                $Files | ForEach-Object {
                     if (-Not (($PSItem.Growth -eq 0) -and (Get-DbcConfigValue skip.database.filegrowthdisabled))) {
                         It "$($PSItem.Database) file $($PSItem.PhysicalName) on $($PSItem.SqlInstance) has free space under threshold" {
                             $PSItem.PercentFree | Should -BeGreaterOrEqual $threshold -Because "free space within the file should be lower than threshold of $threshold%"
