adc922a88ea7d9a90056f6686816304be50a8691
Michał Poręba <michalporeba@gmail.com>
2018-02-28 06:30:54 +0000

    moved the test database creation function to the internal/functions

--- tests/checks/Test.Database.Tests.ps1
+++ tests/checks/Test.Database.Tests.ps1
@@ -1,22 +1,10 @@
 $commandname = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
+. "$PSScriptRoot/../../internal/functions/Set-DatabaseForIntegrationTesting.ps1"
 
-$sqlinstance = "localhost", "localhost"
-$testdb = "DbcChecksTestDb"
 $checksScript = "./../checks/Database.Tests.ps1"
-
-function Set-TestDatabase {
-    param (
-        [DbaInstanceParameter]$SqlInstance
-    )
-    process {
-        $db = Get-DbaDatabase -SqlInstance $instance -SqlCredential $sqlcredential -Database $testdb 
-        if ($db -eq $null) {
-            $server = Connect-DbaInstance -SqlInstance $SqlInstance -SqlCredential $sqlcredential
-            $server.Query("create database $testdb")
-        }
-    }
-}
+$sqlinstance = "localhost\SQL2017DEV", "localhost\SQL2016STD"
+$testdb = "DbcChecksTestDb"
 
 Describe "Tests database checks" {
     # there is no need to use Get-Instance as it is not the Invoke-DbcCheck run
@@ -24,16 +12,16 @@ Describe "Tests database checks" {
     $sqlinstance.ForEach{
         Context "Validate database collation check on $psitem" {
             BeforeAll {
-                Set-TestDatabase -SqlInstance $psitem -SqlCredential $sqlcredential
-                $serverCollation = (Invoke-DbaSqlCmd -SqlInstance $psitem -SqlCredential $sqlcredential -Query "select serverproperty('Collation') ServerCollation")[0]
+                Set-DatabaseForIntegrationTesting -SqlInstance $psitem -SqlCredential $sqlcredential -DatabaseName $testdb 
+                $serverCollation = (Invoke-DbaSqlQuery -SqlInstance $psitem -SqlCredential $sqlcredential -Query "select serverproperty('Collation') ServerCollation")[0]
             }
             It "The test should pass when the database collation matches the instance collation" {
-                Invoke-DbaSqlCmd -SqlInstance $psitem -SqlCredential $sqlcredential -Query "alter database $testdb collate $serverCollation"
+                Invoke-DbaSqlQuery -SqlInstance $psitem -SqlCredential $sqlcredential -Query "alter database $testdb collate $serverCollation"
                 { Invoke-DbcCheck -Tags DatabaseCollation -Script $checksScript -SqlInstance $psitem -SqlCredential $sqlcredential -Show None -PassThru } | Should -Not -Throw -Because "the collations match and the test failed to recognise it"
             }
             It "The test should fail when the database collation doesn't match the instance collation" {
                 # Thai_CI_AS collation seems to be 'exotic' enough not to be the default server collation in any test environment. 
-                Invoke-DbaSqlCmd -SqlInstance $psitem -SqlCredential $sqlcredential -Query "alter database $testdb collate Thai_CI_AS"
+                Invoke-DbaSqlQuery -SqlInstance $psitem -SqlCredential $sqlcredential -Query "alter database $testdb collate Thai_CI_AS"
                 { Invoke-DbaCheck -Tags DatabaseCollation -Script $checksScript -SqlInstance $psitem -SqlCredential $sqlcredential -Show None -PassThru } | Should -Throw -Because "the collations don't match but the test failed to fail"
             }
         }
