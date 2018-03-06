58979f1e13dd5c26a0dfe63e679ac6a1c6e88082
potatoqualitee <clemaire@gmail.com>
2018-03-06 15:52:09 +0100

    force array

--- internal/functions/Get-SqlInstance.ps1
+++ internal/functions/Get-SqlInstance.ps1
@@ -1,14 +1,14 @@
 ﻿function Get-SqlInstance {
     if ($null -ne $sqlinstance) {
-        return @($sqlinstance)
+        return [array]$sqlinstance
+    }
+    
+    $instances = Get-DbcConfigValue app.sqlinstance
+    if ($instances.Length -eq 0) {
+        Write-PSFMessage -Level Warning -Message "You must specify -SqlInstance or use Set-DbcConfig app.sqlinstance to setup a list of servers"
+        return
     }
     else {
-        $instances = Get-DbcConfigValue app.sqlinstance
-        if ($instances.Count -eq 0) {
-            Write-PSFMessage -Level Warning -Message "You must specify -SqlInstance or use Set-DbcConfig app.sqlinstance to setup a list of servers"
-        }
-        else {
-            @($instances)
-        }
+        [array]$instances
     }
 }
