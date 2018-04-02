ef0cd7639ca3a09da310404dfd513484b67e90b7
Michał Poręba <michalporeba@gmail.com>
2018-04-02 10:57:46 +0100

    configurable integration tests instances

--- tests/Invoke-DbcCheck.Tests.ps1
+++ tests/Invoke-DbcCheck.Tests.ps1
@@ -2,11 +2,13 @@
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
 . "$PSScriptRoot\constants.ps1"
 
-Describe "$commandname Integration Tests" -Tags "IntegrationTests" {
-    Context "Command executes properly and returns proper info" {
-        It "runs a check" {
-            $results = Invoke-DbcCheck -ComputerName localhost -Tag DiskCapacity -Passthru -Show None
-            $results.TestResult | Should Not Be $null # Because nothing else works right now
+Describe "$commandname Integration Tests" -Tags IntegrationTests {
+    @(Get-DbcConfigValue testing.integration.instance).ForEach{
+        Context "Command executes properly and returns proper info on $psitem" {
+            It "runs a check" {
+                $results = Invoke-DbcCheck -ComputerName localhost -Tag DiskCapacity -Passthru -Show None
+                $results.TestResult | Should Not Be $null # Because nothing else works right now
+            }
         }
     }
 }
