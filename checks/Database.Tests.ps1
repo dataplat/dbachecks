f1fa17b5dfc18f870edbb76dd8595b23079799da
Michał Poręba <michalporeba@gmail.com>
2018-04-06 21:15:17 +0100

    backup checks in a dedicated files

--- checks/Database.Tests.ps1
+++ checks/Database.Tests.ps1
@@ -10,7 +10,7 @@
  # /tests/assertions/Database.<CheckName>.Tests.ps1         - where the assertions are unit tested
  #>
 
- $filename = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
+$filename = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 
 . "$PSScriptRoot/../internal/functions/Get-DatabaseInfo.ps1"
 
@@ -185,45 +185,6 @@ Describe "Database Owner is valid" -Tags ValidDatabaseOwner, FastDatabase, $file
 
 # Still to be reviewed
 
-Describe "Last Backup Restore Test" -Tags TestLastBackup, Backup, $filename {
-    if (-not (Get-DbcConfigValue skip.backup.testing)) {
-        $destserver = Get-DbcConfigValue policy.backup.testserver
-        $destdata = Get-DbcConfigValue policy.backup.datadir
-        $destlog = Get-DbcConfigValue policy.backup.logdir
-        @(Get-Instance).ForEach{
-            Context "Testing Backup Restore & Integrity Checks on $psitem" {
-            @(Test-DbaLastBackup -SqlInstance $psitem -Database ((Connect-DbaInstance -SqlInstance $psitem).Databases.Where{$_.CreateDate -lt (Get-Date).AddHours( - $graceperiod) -and ($ExcludedDatabases -notcontains $PsItem.Name)}).Name -VerifyOnly).ForEach{
-                    
-                    if ($psitem.DBCCResult -notmatch "skipped for restored master") {
-                        It "DBCC for $($psitem.Database) on $($psitem.SourceServer) Should Be success" {
-                            $psitem.DBCCResult | Should -Be "Success" -Because "You need to run DBCC CHECKDB to ensure your database is consistent"
-                        }
-                        It "restore for $($psitem.Database) on $($psitem.SourceServer) Should Be success" {
-                            $psitem.RestoreResult | Should -Be "Success" -Because "The backup file has not successfully restored - you have no backup"
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-Describe "Last Backup VerifyOnly" -Tags TestLastBackupVerifyOnly, Backup, $filename {
-    $graceperiod = Get-DbcConfigValue policy.backup.newdbgraceperiod
-    @(Get-Instance).ForEach{
-        Context "VerifyOnly tests of last backups on $psitem" {
-            @(Test-DbaLastBackup -SqlInstance $psitem -Database ((Connect-DbaInstance -SqlInstance $psitem).Databases.Where{$_.CreateDate -lt (Get-Date).AddHours( - $graceperiod) -and ($ExcludedDatabases -notcontains $PsItem.Name)}).Name -VerifyOnly).ForEach{
-                It "restore for $($psitem.Database) on $($psitem.SourceServer) Should be success" {
-                    $psitem.RestoreResult | Should -Be "Success" -Because "The restore file has not successfully restored - you have no backup"
-                }
-                It "file exists for last backup of $($psitem.Database) on $($psitem.SourceServer)" {
-                    $psitem.FileExists | Should -BeTrue -Because "Without a backup file you have no backup"
-                }
-            }
-        }
-    }
-}
-
 Describe "Last Good DBCC CHECKDB" -Tags LastGoodCheckDb, $filename {
     $maxdays = Get-DbcConfigValue policy.dbcc.maxdays
     $datapurity = Get-DbcConfigValue skip.dbcc.datapuritycheck
@@ -319,38 +280,6 @@ Describe "Database Growth Event" -Tags DatabaseGrowthEvent, $filename {
     }
 }
 
-Describe "Last Full Backup Times" -Tags LastFullBackup, LastBackup, Backup, DISA, $filename {
-    $maxfull = Get-DbcConfigValue policy.backup.fullmaxdays
-    $graceperiod = Get-DbcConfigValue policy.backup.newdbgraceperiod
-    @(Get-Instance).ForEach{
-        Context "Testing last full backups on $psitem" {
-            @((Connect-DbaInstance -SqlInstance $psitem).Databases.Where{ ($psitem.Name -ne 'tempdb') -and $Psitem.CreateDate -lt (Get-Date).AddHours( - $graceperiod) -and ($ExcludedDatabases -notcontains $PsItem.Name)}).ForEach{
-                $offline = ($psitem.Status -match "Offline")
-                It -Skip:$offline "$($psitem.Name) full backups on $($psitem.Parent.Name) Should Be less than $maxfull days" {
-                    $psitem.LastBackupDate | Should -BeGreaterThan (Get-Date).AddDays( - ($maxfull)) -Because "Taking regular backups is extraordinarily important"
-                }
-            }
-        }
-    }
-}
-
-Describe "Last Diff Backup Times" -Tags LastDiffBackup, LastBackup, Backup, DISA, $filename {
-    if (-not (Get-DbcConfigValue skip.diffbackuptest)) {
-        $maxdiff = Get-DbcConfigValue policy.backup.diffmaxhours
-        $graceperiod = Get-DbcConfigValue policy.backup.newdbgraceperiod
-        @(Get-Instance).ForEach{
-            Context "Testing last diff backups on $psitem" {
-                @((Connect-DbaInstance -SqlInstance $psitem).Databases.Where{ (-not $psitem.IsSystemObject) -and $Psitem.CreateDate -lt (Get-Date).AddHours( - $graceperiod) -and ($ExcludedDatabases -notcontains $PsItem.Name)}).ForEach{
-                    $offline = ($psitem.Status -match "Offline")
-                    It -Skip:$offline "$($psitem.Name) diff backups on $($psitem.Parent.Name) Should Be less than $maxdiff hours" {
-                        $psitem.LastDifferentialBackupDate | Should -BeGreaterThan (Get-Date).AddHours( - ($maxdiff)) -Because 'Taking regular backups is extraordinarily important'
-                    }
-                }
-            }
-        }
-    }
-}
-
 Describe "Last Log Backup Times" -Tags LastLogBackup, LastBackup, Backup, DISA, $filename {
     $maxlog = Get-DbcConfigValue policy.backup.logmaxminutes
     $graceperiod = Get-DbcConfigValue policy.backup.newdbgraceperiod
@@ -440,7 +369,6 @@ Describe "Future File Growth" -Tags FutureFileGrowth, $filename {
         }
     }
 }
-
 
 Describe "Correctly sized Filegroup members" -Tags FileGroupBalanced, $filename {
     $Tolerance = Get-DbcConfigValue policy.database.filebalancetolerance
