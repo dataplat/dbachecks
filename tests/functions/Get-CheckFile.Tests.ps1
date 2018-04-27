1cf542d5d884721340706f3ec6bc85f9392a9016
Rob Sewell <mrrobsewell@outlook.com>
2018-04-27 11:22:45 +0100

    updated path to file

--- tests/functions/Get-CheckFile.Tests.ps1
+++ tests/functions/Get-CheckFile.Tests.ps1
@@ -1,6 +1,6 @@
 $commandname = $MyInvocation.MyCommand.Name.Replace(".Tests.ps1", "")
 Write-Host -Object "Running $PSCommandpath" -ForegroundColor Cyan
-. "$PSScriptRoot/../internal/functions/Get-CheckFile.ps1"
+. "$PSScriptRoot/../../internal/functions/Get-CheckFile.ps1"
 
 Describe "Testing Get-CheckFile function" {
     Mock Get-ChildItem { 
