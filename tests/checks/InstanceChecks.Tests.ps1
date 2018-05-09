8cc80ef001aa4d944eda5383d0ec645855925c9e
Rob Sewell <mrrobsewell@outlook.com>
2018-05-09 09:24:50 +0100

    need the right variable name

--- tests/checks/InstanceChecks.Tests.ps1
+++ tests/checks/InstanceChecks.Tests.ps1
@@ -7,11 +7,11 @@ Describe "Checking Instance.Tests.ps1 checks" -Tag UnitTest {
         Mock Connect-DbaInstance {@{Version = @{Major = 14}}}
         # Define test cases for the results to fail test and fill expected message
         # So the results of SPConfigure is 1, we expect $false but the result is true and the results of SPConfigure is 0, we expect $true but the result is false
-        $TestCases = @{spconfig= 1;expected = $false;result = $true},@{spconfig= 0;expected = $true;result = $false}
+        $TestCases = @{spconfig= 1;expected = $false;actual = $true},@{spconfig= 0;expected = $true;actual = $false}
         It "Fails Check Correctly for Config <spconfig> and expected value <expected>" -TestCases $TestCases {
             Param($spconfig, $actual, $expected)
             Mock Get-DbaSpConfigure {@{"ConfiguredValue" = $spconfig}}
-        {Assert-BackupCompression -Instance 'Dummy' -defaultbackupcompression $expected} | Should -Throw -ExpectedMessage "Expected `$$expected, because The default backup compression should be set correctly, but got `$$result"
+        {Assert-BackupCompression -Instance 'Dummy' -defaultbackupcompression $expected} | Should -Throw -ExpectedMessage "Expected `$$expected, because The default backup compression should be set correctly, but got `$$actual"
         }
         $TestCases = @{spconfig= 0;expected = $false},@{spconfig= 1;expected = $true;}
         It "Passes Check Correctly for Config <spconfig> and expected value <expected>" -TestCases $TestCases {
