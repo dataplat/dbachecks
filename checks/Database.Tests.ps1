15a02836c230130e752b53e88b50da2bce3e6943
Rob Sewell <mrrobsewell@outlook.com>
2018-05-09 08:43:12 +0100

    logic for skip

--- checks/Database.Tests.ps1
+++ checks/Database.Tests.ps1
@@ -257,10 +257,11 @@ Describe "Auto Shrink" -Tags AutoShrink, $filename {
 Describe "Last Full Backup Times" -Tags LastFullBackup, LastBackup, Backup, DISA, $filename {
     $maxfull = Get-DbcConfigValue policy.backup.fullmaxdays
     $graceperiod = Get-DbcConfigValue policy.backup.newdbgraceperiod
+    $skipreadonly = Get-DbcConfigValue skip.backup.readonly
     @(Get-Instance).ForEach{
         Context "Testing last full backups on $psitem" {
             @((Connect-DbaInstance -SqlInstance $psitem).Databases.Where{ ($psitem.Name -ne 'tempdb') -and $Psitem.CreateDate -lt (Get-Date).AddHours( - $graceperiod) -and ($ExcludedDatabases -notcontains $PsItem.Name)}).ForEach{
-                $skip = ($psitem.Status -match "Offline")
+                $skip = ($psitem.Status -match "Offline") -or ($psitem.Readonly  -eq $true -and $skipreadonly -eq $true)
                 It -Skip:$skip "$($psitem.Name) full backups on $($psitem.Parent.Name) Should Be less than $maxfull days" {
                     $psitem.LastBackupDate | Should -BeGreaterThan (Get-Date).AddDays( - ($maxfull)) -Because "Taking regular backups is extraordinarily important"
                 }
@@ -273,10 +274,11 @@ Describe "Last Diff Backup Times" -Tags LastDiffBackup, LastBackup, Backup, DISA
     if (-not (Get-DbcConfigValue skip.diffbackuptest)) {
         $maxdiff = Get-DbcConfigValue policy.backup.diffmaxhours
         $graceperiod = Get-DbcConfigValue policy.backup.newdbgraceperiod
+        $skipreadonly = Get-DbcConfigValue skip.backup.readonly
         @(Get-Instance).ForEach{
             Context "Testing last diff backups on $psitem" {
                 @((Connect-DbaInstance -SqlInstance $psitem).Databases.Where{ (-not $psitem.IsSystemObject) -and $Psitem.CreateDate -lt (Get-Date).AddHours( - $graceperiod) -and ($ExcludedDatabases -notcontains $PsItem.Name)}).ForEach{
-                    $skip = ($psitem.Status -match "Offline")
+                    $skip = ($psitem.Status -match "Offline") -or ($psitem.Readonly  -eq $true -and $skipreadonly -eq $true)
                     It -Skip:$skip "$($psitem.Name) diff backups on $($psitem.Parent.Name) Should Be less than $maxdiff hours" {
                         $psitem.LastDifferentialBackupDate | Should -BeGreaterThan (Get-Date).AddHours( - ($maxdiff)) -Because 'Taking regular backups is extraordinarily important'
                     }
@@ -289,11 +291,12 @@ Describe "Last Diff Backup Times" -Tags LastDiffBackup, LastBackup, Backup, DISA
 Describe "Last Log Backup Times" -Tags LastLogBackup, LastBackup, Backup, DISA, $filename {
     $maxlog = Get-DbcConfigValue policy.backup.logmaxminutes
     $graceperiod = Get-DbcConfigValue policy.backup.newdbgraceperiod
+    $skipreadonly = Get-DbcConfigValue skip.backup.readonly
     @(Get-Instance).ForEach{
         Context "Testing last log backups on $psitem" {
             @((Connect-DbaInstance -SqlInstance $psitem).Databases.Where{ (-not $psitem.IsSystemObject) -and $Psitem.CreateDate -lt (Get-Date).AddHours( - $graceperiod) -and ($ExcludedDatabases -notcontains $PsItem.Name)}).ForEach{
                 if ($psitem.RecoveryModel -ne "Simple") {
-                    $skip = ($psitem.Status -match "Offline")
+                    $skip = ($psitem.Status -match "Offline") -or ($psitem.Readonly  -eq $true -and $skipreadonly -eq $true)
                     It -Skip:$skip  "$($psitem.Name) log backups on $($psitem.Parent.Name) Should Be less than $maxlog minutes" {
                         $psitem.LastLogBackupDate | Should -BeGreaterThan (Get-Date).AddMinutes( - ($maxlog) + 1) -Because "Taking regular backups is extraordinarily important"
                     }
