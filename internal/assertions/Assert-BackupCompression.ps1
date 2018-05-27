411405a94715d57cb433cc6e9ec73d19392aabfa
Rob Sewell <mrrobsewell@outlook.com>
2018-05-27 09:23:16 +0100

    Easier to manage in one file and quicker to load

--- internal/assertions/Assert-BackupCompression.ps1
+++
@@ -1,4 +0,0 @@
-function Assert-BackupCompression {
-    Param($Instance,$defaultbackupcompression)
-    (Get-DbaSpConfigure -SqlInstance $Instance -ConfigName 'DefaultBackupCompression').ConfiguredValue -eq 1 | Should -Be $defaultbackupcompression -Because 'The default backup compression should be set correctly'
-}
--- internal/assertions/Assert-DatabaseMaxDop.ps1
+++
@@ -1,7 +0,0 @@
-function Assert-DatabaseMaxDop {
-    Param(
-        [pscustomobject]$MaxDop,
-        [int]$MaxDopValue
-    )   
-    $MaxDop.DatabaseMaxDop | Should -Be $MaxDopValue -Because "We expect the Database MaxDop Value $($MaxDop.DatabaseMaxDop) to be the specified value $MaxDopValue"
-}
