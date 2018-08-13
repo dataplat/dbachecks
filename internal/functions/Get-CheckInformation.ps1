ad461102fa178979e90eb2df23e9fd6fd733207c
Rob Sewell <Rob.Sewell@Harrogate.Jet2.com>
2018-08-13 16:13:13 +0100

    Gather hte checks in use in the file

--- internal/functions/Get-CheckInformation.ps1
+++ internal/functions/Get-CheckInformation.ps1
@@ -1,17 +1,33 @@
+# Returns all of the tags that have been specified in the checks file for this run of the Invoke-DbcCheck
 function Get-CheckInformation {
-    Param($Group)
+    Param($Group, $Check, $AllChecks ,$ExcludeCheck)
+    ## need to reset the variable here
     $script:localapp = Get-DbcConfigValue -Name app.localapp
     $GroupChecksConfig = (Get-DbcCheck).Where{$_.Group -eq $Group}
+    # Nothing if we exclude the group
     if ($ExcludeCheck -contains $Group) {Return}
-        $GroupChecks = @()
-        @($GroupChecksConfig.AllTags).foreach{
-            @($psitem.Split(',')).ForEach{
-                $checkarray = $_.Trim()
-                if ($checkarray -eq $Group) {}
-                elseif ($ExcludeCheck -match $Checkarray) {}
-                else {
-                    $GroupChecks += $checkarray
-                }
+    # Create an array of tags for the group except the Group Name
+    $GroupChecks = @()
+    @($GroupChecksConfig.AllTags).foreach{
+        @($psitem.Split(',')).ForEach{
+            $checkitem = $_.Trim()
+            if ($checkitem -eq $Group) {}
+            elseif ($ExcludeCheck -match $Checkitem) {}
+            else {
+                $GroupChecks += $checkitem
             }
         }
+    }
+    $CheckInfo = @()
+    if(($Check -eq $Group) -or ($Check -contains $Group) -or ($AllChecks)){
+        $CheckInfo = $GroupChecks
+    }
+    else{
+        @($Check).ForEach{
+            if($GroupChecks -contains $psitem){
+                $CheckInfo += $psitem
+            }
+        }
+    }
+    Return $CheckInfo 
 }
