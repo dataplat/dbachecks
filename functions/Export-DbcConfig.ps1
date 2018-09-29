65d0a29642aba62f68382fe80225713aecf29b64
Rob Sewell <mrrobsewell@outlook.com>
2018-09-29 18:32:42 +0100

    updated help for online help

--- functions/Export-DbcConfig.ps1
+++ functions/Export-DbcConfig.ps1
@@ -1,29 +1,31 @@
-﻿function Export-DbcConfig {
-    <#
-        .SYNOPSIS
-            Exports dbachecks configs from a json file to make it easier to modify or be used for specific configurations.
-        
-        .DESCRIPTION
-            Exports dbachecks configs from a json file to make it easier to modify or be used for specific configurations.
+﻿<#
+.SYNOPSIS
+    Exports dbachecks configs from a json file to make it easier to modify or be used for specific configurations.
+
+.DESCRIPTION
+    Exports dbachecks configs from a json file to make it easier to modify or be used for specific configurations.
+
+.PARAMETER Path
+    The path to export to, by default is "$script:localapp\config.json"
+
+.PARAMETER EnableException
+By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+.EXAMPLE
+    Export-DbcConfig
     
-        .PARAMETER Path
-            The path to export to, by default is "$script:localapp\config.json"
+    Exports config to "$script:localapp\config.json"
+.EXAMPLE
+    Export-DbcConfig -Path \\nfs\projects\config.json
     
-        .PARAMETER EnableException
-        By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-        This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-        Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
-        
-        .EXAMPLE
-            Export-DbcConfig
-            
-            Exports config to "$script:localapp\config.json"
-    
-        .EXAMPLE
-            Export-DbcConfig -Path \\nfs\projects\config.json
-            
-            Exports config to \\nfs\projects\config.json
-    #>
+    Exports config to \\nfs\projects\config.jso
+
+.LINK
+https://dbachecks.readthedocs.io/en/latest/functions/Export-DbcConfig/
+#>
+function Export-DbcConfig {
     [CmdletBinding()]
     param (
         [string]$Path = "$script:localapp\config.json",
