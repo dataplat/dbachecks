d5fd1c29eefd9f958b438bd8c9dad0c896c61437
Rob Sewell <mrrobsewell@outlook.com>
2018-09-29 18:26:56 +0100

    updated the examples

--- functions/Set-DbcConfig.ps1
+++ functions/Set-DbcConfig.ps1
@@ -1,46 +1,62 @@
-﻿function Set-DbcConfig {
-    <#
-        .SYNOPSIS
-            Sets configuration entries.
-    
-        .DESCRIPTION
-            This function creates or changes configuration values.
+﻿<#
+.SYNOPSIS
+Sets configuration values for specific checks.
 
-            These can be used to provide dynamic configuration information outside the PowerShell variable system.
-    
-        .PARAMETER Name
-            Name of the configuration entry.
-    
-        .PARAMETER Value
-            The value to assign to the named configuration element.
-    
-        .PARAMETER Handler
-            A scriptblock that is executed when a value is being set.
+.DESCRIPTION
+Changes configuration values which enable each check to have specific threshholds
 
-            Is only executed if the validation was successful (assuming there was a validation, of course)
-    
-        .PARAMETER Append
-            Adds the value to the existing configuration instead of overwriting it
-    
-        .PARAMETER Temporary
-            The setting is not persisted outside the current session.
-            By default, settings will be remembered across all powershell sessions.
-    
-        .PARAMETER EnableException
-            By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-            This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-            Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
-            
-        .EXAMPLE
-            Set-DbcConfig -Name Lists.SqlServers -Value sql2016, sql2017, sqlcluster
-        
-            Resets the lists.sqlservers entry to sql2016, sql2017, sqlcluster
-    
-        .EXAMPLE
-            Set-DbcConfig -Name Lists.SqlServers -Value sql2016, sql2017, sqlcluster -Append
-        
-            Addds on to the current lists.sqlservers entry with sql2016, sql2017, sqlcluster
-    #>
+.PARAMETER Name
+Name of the configuration entry.
+
+.PARAMETER Value
+The value to assign.
+
+.PARAMETER Handler
+A scriptblock that is executed when a value is being set.
+
+Is only executed if the validation was successful (assuming there was a validation, of course)
+
+.PARAMETER Append
+Adds the value to the existing configuration instead of overwriting it
+
+.PARAMETER Temporary
+The setting is not persisted outside the current session.
+By default, settings will be remembered across all powershell sessions.
+
+.PARAMETER EnableException
+By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+
+.EXAMPLE
+Set-DbcConfig -Name app.sqlinstance -Value sql2016, sql2017, sqlcluster
+
+Sets the SQL Instances which will be checked by default using Invoke-DbcCheck 
+to sql2016, sql2017, sqlcluster
+
+.EXAMPLE
+Set-DbcConfig -Name policy.validdbowner.name -Value 'TheBeard\sqldbowner' 
+
+Sets the value of the configuration for the expected database owners to
+TheBeard\sqldbowner
+
+.EXAMPLE
+Set-DbcConfig -Name policy.database.status.excludereadonly -Value 'TheBeard' 
+
+Sets the value of the configuration for databases that are expected to be readonly
+to TheBeard
+
+.EXAMPLE
+Set-DbcConfig -Name agent.validjobowner.name -Value 'TheBeard\SQLJobOwner' -Append
+
+Adds 'TheBeard\SQLJobOwner' to the value of the configuration for accounts that 
+are expected to be owners of SQL Agent Jobs 
+
+.LINK
+https://dbachecks.readthedocs.io/en/latest/functions/Set-DbcConfig/
+
+#>
+function Set-DbcConfig {
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseShouldProcessForStateChangingFunctions", "")]
     [CmdletBinding(DefaultParameterSetName = "FullName")]
     param (
@@ -60,31 +76,32 @@
             Stop-PSFFunction -Message "Setting named $Name does not exist. If you'd like us to support an additional setting, please file a GitHub issue."
             return
         }
-        
+
         if ($Append) {
             $NewValue = (Get-DbcConfigValue -Name $Name)
 
             # this is important to fix issue 535
             # Need to process arrays correctly
-            if($NewValue -is [System.Array]){
-                if($value -is [System.Array]){
+            if ($NewValue -is [System.Array]) {
+                if ($value -is [System.Array]) {
                     $Value.ForEach{
                         $NewValue += $psitem
                     }
                 }
-                else{
+                else {
                     $NewValue += $Value
                 }
             }
-            else{
+            else {
                 $NewValue = $NewValue, $Value
             }
-        }else{
+        }
+        else {
             $NewValue = $Value
         }
-        
+
         $Name = $Name.ToLower()
-        
+
         Set-PSFConfig -Module dbachecks -Name $name -Value $NewValue
         try {
             if (-not $Temporary) { Register-PSFConfig -FullName dbachecks.$name -EnableException -WarningAction SilentlyContinue }
@@ -93,12 +110,12 @@
             Set-PSFConfig -Module dbachecks -Name $name -Value ($Value -join ", ")
             if (-not $Temporary) { Register-PSFConfig -FullName dbachecks.$name }
         }
-        
+
         # Still unsure if I'll persist it here - wondering if this impacts global or keeps local
         if ($name -eq 'app.sqlcredential') {
             Set-Variable -Scope 1 -Name PSDefaultParameterValues -Value @{ '*:SqlCredential' = $value }
         }
-        
+
         Get-DbcConfig -Name $name
     }
 }
