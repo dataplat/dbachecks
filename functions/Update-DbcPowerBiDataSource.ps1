5f368931c2d5acd95abf883afde0f371e45b16e0
Rob Sewell <mrrobsewell@outlook.com>
2018-09-29 18:33:53 +0100

    updated help and online help fiel

--- functions/Update-DbcPowerBiDataSource.ps1
+++ functions/Update-DbcPowerBiDataSource.ps1
@@ -1,72 +1,81 @@
-﻿function Update-DbcPowerBiDataSource {
-    <#
-        .SYNOPSIS
-            Converts Pester results and exports file in required format for launching the Power BI command. **You will need refresh* the Power BI dashboard every time to see the new results.
+﻿<#
+.SYNOPSIS
+Converts Pester results and exports file in required format for launching the 
+Power BI command. **You will need refresh* the Power BI dashboard every time to 
+see the new results.
 
-        .DESCRIPTION
-            Converts Pester results and exports file in required format for launching the Power BI command. **You will need refresh* the Power BI dashboard every time to see the new results.
+.DESCRIPTION
+Converts Pester results and exports file in required format for launching the 
+Power BI command. **You will need refresh* the Power BI dashboard every time to 
+see the new results.
 
-            Basically does this:
-                $InputObject.TestResult | Select-Object -First 20 | ConvertTo-Json -Depth 3 | Out-File "$env:windir\temp\dbachecks.json"
+Basically, it does this:
+$InputObject.TestResult | Select-Object -First 20 | ConvertTo-Json -Depth 3 | Out-File "$env:windir\temp\dbachecks.json"
 
-        .PARAMETER InputObject
-            Required. Resultset from Invoke-DbcCheck. If InputObject is not provided, it will be generated using a very generic resultset:
+.PARAMETER InputObject
+Required. Resultset from Invoke-DbcCheck. If InputObject is not provided, it will be generated using a very generic resultset:
 
-            Invoke-DbcCheck -Show Summary -PassThru
+.PARAMETER Path
+The directory to store your JSON files. "C:\windows\temp\dbachecks\*.json" by default
 
-        .PARAMETER Path
-            The directory to store your JSON files. "C:\windows\temp\dbachecks\*.json" by default
+.PARAMETER FileName
+if you want to give the file a specific name
 
-        .PARAMETER FileName
-            Name your Json File
+.PARAMETER Environment
+A Name to give your suite of tests IE Prod - This will also alter the name of the file
 
-        .PARAMETER Environment
-            A Naem to give your suite of tests IE Prod - This will also alter the name
-    
-        .PARAMETER Force
-            Delete all json files in the data source folder.
-    
-        .PARAMETER EnableException
-            By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
-            This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
-            Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
+.PARAMETER Force
+Delete all json files in the data source folder.
 
-        .EXAMPLE
-            Invoke-DbcCheck -SqlInstance $Instance -Check DatabaseStatus -Show None -PassThru | Update-DbcPowerBiDataSource 
+.PARAMETER EnableException
+By default, when something goes wrong we try to catch it, interpret it and give you a friendly warning message.
+This avoids overwhelming you with "sea of red" exceptions, but is inconvenient because it basically disables advanced scripting.
+Using this switch turns this "nice by default" feature off and enables you to catch exceptions with your own try/catch.
 
-            Runs the DatabaseStatus checks against $Instance then saves to json to $env:windir\temp\dbachecks\dbachecks_1_DatabaseStatus.json
+.EXAMPLE
+Invoke-DbcCheck -SqlInstance $Instance -Check DatabaseStatus -Show None -PassThru | Update-DbcPowerBiDataSource 
 
-        .EXAMPLE
-            Invoke-DbcCheck -SqlInstance $Instance -Check DatabaseStatus -Show None -PassThru | Update-DbcPowerBiDataSource -Path C:\Temp
+Runs the DatabaseStatus checks against $Instance then saves to json to $env:windir\temp\dbachecks\dbachecks_1_DatabaseStatus.json
 
-            Runs the DatabaseStatus checks against $Instance then saves to json to C:\Temp\dbachecks_1_DatabaseStatus.json
+.EXAMPLE
+Invoke-DbcCheck -SqlInstance $Instance -Check DatabaseStatus -Show None -PassThru | Update-DbcPowerBiDataSource -Path C:\Temp
 
-        .EXAMPLE
-            Invoke-DbcCheck -SqlInstance $Instance -Check DatabaseStatus -Show None -PassThru | Update-DbcPowerBiDataSource -Path C:\Temp  -FileName BeardyTests
+Runs the DatabaseStatus checks against $Instance then saves to json to C:\Temp\dbachecks_1_DatabaseStatus.json
 
-            Runs the DatabaseStatus checks against $Instance then saves to json to C:\Temp\BeardyTests.json
+.EXAMPLE
+Invoke-DbcCheck -SqlInstance $Instance -Check DatabaseStatus -Show None -PassThru | Update-DbcPowerBiDataSource -Path C:\Temp  -FileName BeardyTests
 
-        .EXAMPLE
-            Invoke-DbcCheck -SqlInstance $Instance -Check DatabaseStatus -Show None -PassThru | Update-DbcPowerBiDataSource -Path C:\Temp  -FileName BeardyTests.json
+Runs the DatabaseStatus checks against $Instance then saves to json to C:\Temp\BeardyTests.json
 
-            Runs the DatabaseStatus checks against $Instance then saves to json to C:\Temp\BeardyTests.json
+.EXAMPLE
+Invoke-DbcCheck -SqlInstance $Instance -Check DatabaseStatus -Show None -PassThru | Update-DbcPowerBiDataSource -Path C:\Temp  -FileName BeardyTests.json
 
-        .EXAMPLE
-            Invoke-DbcCheck -SqlInstance $Instance -Check DatabaseStatus -Show None -PassThru | Update-DbcPowerBiDataSource -Path C:\Temp  -Environment Prod_DBChecks
+Runs the DatabaseStatus checks against $Instance then saves to json to C:\Temp\BeardyTests.json
 
-            Runs the DatabaseStatus checks against $Instance then saves to json to  C:\Temp\dbachecks_1_Prod_DBChecks_DatabaseStatus.json
+.EXAMPLE
+Invoke-DbcCheck -SqlInstance $Instance -Check DatabaseStatus -Show None -PassThru | Update-DbcPowerBiDataSource -Path C:\Temp  -Environment Prod_DBChecks
 
-        .EXAMPLE
-            Invoke-DbcCheck -SqlInstance $Instance -Check DatabaseStatus -Show None -PassThru | Update-DbcPowerBiDataSource -Environment Prod_DBChecks
+Runs the DatabaseStatus checks against $Instance then saves to json to  C:\Temp\dbachecks_1_Prod_DBChecks_DatabaseStatus.json
 
-            Runs the DatabaseStatus checks against $Instance then saves to json to  C:\Windows\temp\dbachecks\dbachecks_1_Prod_DBChecks_DatabaseStatus.json
+.EXAMPLE
+Invoke-DbcCheck -SqlInstance $Instance -Check DatabaseStatus -Show None -PassThru | Update-DbcPowerBiDataSource -Environment Prod_DBChecks
 
-        .EXAMPLE
-            Invoke-DbcCheck -SqlInstance sql2017 -Tag Backup -Show Summary -PassThru | Update-DbcPowerBiDataSource -Path \\nas\projects\dbachecks.json
-            Start-DbcPowerBi -Path \\nas\projects\dbachecks.json
+Runs the DatabaseStatus checks against $Instance then saves to json to  C:\Windows\temp\dbachecks\dbachecks_1_Prod_DBChecks_DatabaseStatus.json
 
-            Runs tests, saves to json to \\nas\projects\dbachecks.json but then you'll have to change your data source in Power BI because by default it points to C:\Windows\Temp (limitation of Power BI)
-    #>
+.EXAMPLE
+Invoke-DbcCheck -SqlInstance sql2017 -Tag Backup -Show Summary -PassThru | Update-DbcPowerBiDataSource -Path \\nas\projects\dbachecks.json
+Start-DbcPowerBi -Path \\nas\projects\dbachecks.json
+
+Runs tests, saves to json to \\nas\projects\dbachecks.json
+Opens the PowerBi using that file
+then you'll have to change your data source in Power BI because by default it 
+points to C:\Windows\Temp (limitation of Power BI)
+
+.LINK
+https://dbachecks.readthedocs.io/en/latest/functions/Update-DbcPowerBiDataSource/
+
+#>
+function Update-DbcPowerBiDataSource {
     [CmdletBinding()]
     param (
         [parameter(ValueFromPipeline, Mandatory)]
@@ -97,7 +106,7 @@
             return
         }
         $basename = "dbachecks_$i"
-        if($Environment){
+        if ($Environment) {
             $basename = "dbachecks_$i" + "_$Environment`_"
         }
         if ($FileName) {
@@ -110,8 +119,8 @@
             }
         }  
 
-        if($basename.EndsWith('.json')){}
-        else{
+        if ($basename.EndsWith('.json')) {}
+        else {
             $basename = $basename + ".json"
         }
 
