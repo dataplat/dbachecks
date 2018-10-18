70ce7c15c9e78e2b69e06054554c441e4a5d116f
Rob Sewell <mrrobsewell@outlook.com>
2018-10-18 07:04:04 +0100

    tests for xpcmdshellena

--- tests/checks/InstanceChecks.Tests.ps1
+++ tests/checks/InstanceChecks.Tests.ps1
@@ -590,5 +590,32 @@ Describe "Checking Instance.Tests.ps1 checks" -Tag UnitTest {
             Assert-MockCalled @assertMockParams
         }
     }
+    Context "Checking XPCmdShell is disabled" {
+        # Mock the version check for running tests
+        Mock Connect-DbaInstance {}
+        # Define test cases for the results to fail test and fill expected message
+        # So the results of SPConfigure is 1, we expect $false but the result is true and the results of SPConfigure is 0, we expect $true but the result is false
+        $TestCases = @{spconfig = 1; expected = $false; actual = $true}, @{spconfig = 0; expected = $true; actual = $false}
+        It "Fails Check Correctly for Config <spconfig> and expected value <expected>" -TestCases $TestCases {
+            Param($spconfig, $actual, $expected)
+            Mock Get-DbaSpConfigure {@{"ConfiguredValue" = $spconfig}}
+            {Assert-XpCmdShellDisabled -SQLInstance 'Dummy' -XpCmdShellDisabled $expected} | Should -Throw -ExpectedMessage "Expected `$$expected, because The XP CmdShell setting should be set correctly, but got `$$actual"
+        }
+        $TestCases = @{spconfig = 0; expected = $false}, @{spconfig = 1; expected = $true; }
+        It "Passes Check Correctly for Config <spconfig> and expected value <expected>" -TestCases $TestCases {
+            Param($spconfig, $expected)
+            Mock Get-DbaSpConfigure {@{"ConfiguredValue" = $spconfig}}
+            Assert-XpCmdShellDisabled -SQLInstance 'Dummy' -XpCmdShellDisabled $expected
+        }
+        # Validate we have called the mock the correct number of times
+        It "Should call the mocks" {
+            $assertMockParams = @{
+                'CommandName' = 'Get-DbaSpConfigure'
+                'Times'       = 4
+                'Exactly'     = $true
+            }
+            Assert-MockCalled @assertMockParams
+        }
+    }
 
 }
