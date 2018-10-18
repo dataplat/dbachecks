d8592fafa6df1a0bf0bd8a7f7a55be7f07192553
Rob Sewell <mrrobsewell@outlook.com>
2018-10-18 07:33:22 +0100

    checking for disabled so this test needs to be the otherway round

--- tests/checks/InstanceChecks.Tests.ps1
+++ tests/checks/InstanceChecks.Tests.ps1
@@ -594,14 +594,16 @@ Describe "Checking Instance.Tests.ps1 checks" -Tag UnitTest {
         # Mock the version check for running tests
         Mock Connect-DbaInstance {}
         # Define test cases for the results to fail test and fill expected message
-        # So the results of SPConfigure is 1, we expect $false but the result is true and the results of SPConfigure is 0, we expect $true but the result is false
-        $TestCases = @{spconfig = 1; expected = $false; actual = $true}, @{spconfig = 0; expected = $true; actual = $false}
+        # This one is different from the others as we are checking for disabled !!
+        # So the results of SPConfigure is 1, we expect $true but the result is false and the results of SPConfigure is 0, we expect $false but the result is true
+        $TestCases = @{spconfig = 1; expected = $true; actual = $false}, @{spconfig = 0; expected = $false; actual = $true}
         It "Fails Check Correctly for Config <spconfig> and expected value <expected>" -TestCases $TestCases {
             Param($spconfig, $actual, $expected)
             Mock Get-DbaSpConfigure {@{"ConfiguredValue" = $spconfig}}
             {Assert-XpCmdShellDisabled -SQLInstance 'Dummy' -XpCmdShellDisabled $expected} | Should -Throw -ExpectedMessage "Expected `$$expected, because The XP CmdShell setting should be set correctly, but got `$$actual"
         }
-        $TestCases = @{spconfig = 0; expected = $false}, @{spconfig = 1; expected = $true; }
+        # again this one is different from the others as we are checking for disabled
+        $TestCases = @{spconfig = 1; expected = $false}, @{spconfig = 0; expected = $true; }
         It "Passes Check Correctly for Config <spconfig> and expected value <expected>" -TestCases $TestCases {
             Param($spconfig, $expected)
             Mock Get-DbaSpConfigure {@{"ConfiguredValue" = $spconfig}}
