8dc6ea8e6f1ce9ba9fec85ca3973811e47a2f644
Rob Sewell <Rob.Sewell@Harrogate.Jet2.com>
2018-11-23 08:40:35 +0000

    need to set the value if there is no Check parameter closes #576

--- internal/functions/Get-CheckInformation.ps1
+++ internal/functions/Get-CheckInformation.ps1
@@ -7,6 +7,7 @@ function Get-CheckInformation {
     # Nothing if we exclude the group
     if ($ExcludeCheck -contains $Group) {Return}
     # Create an array of tags for the group except the Group Name
+    #It's a bit clubnky but it works
     $GroupChecks = @()
     @($GroupChecksConfig.AllTags).foreach{
         @($psitem.Split(',')).ForEach{
@@ -18,6 +19,20 @@ function Get-CheckInformation {
             }
         }
     }
+    # Now we have a list of tags if we have a group. We need to have a list of tags for all the possible checks passed in
+
+    #BUT
+
+    # If we have called Invoke-DbcCheck without using the Check parameter (IE using the config) we have nothing in Check
+
+    # So Lets fix that!
+
+    if($Check -eq $null){
+        $Check = $GroupChecks
+    }
+
+    ## OK - Now we can return all of the tags for all fo the checks whether they are specified individually, by group, in the Check parameter or not specified and included by the config as either a group or an individual tag (Which is what I want!)
+    
     $CheckInfo = @()
     if(($Check -eq $Group) -or ($Check -contains $Group) -or ($AllChecks)){
         $CheckInfo = $GroupChecks
