3ea9c843aa9a42e525fe95688fbd82772ed0d090
Cláudio Silva <claudiosil100@gmail.com>
2018-11-30 08:06:23 +0000

    Add Assert functions

--- internal/assertions/Agent.Assertions.ps1
+++ internal/assertions/Agent.Assertions.ps1
@@ -5,3 +5,27 @@ function Assert-DatabaseMailEnabled {
     )
    (Get-DbaSpConfigure -SqlInstance $SQLInstance -Name DatabaseMailEnabled).ConfiguredValue -eq 1 | Should -Be $DatabaseMailEnabled -Because 'The Database Mail XPs setting should be set correctly'
 }
+
+function Assert-JobHistoryRowsDisabled {
+    param (
+        $AgentServer,
+        $minimumJobHistoryRows
+    )
+    $AgentServer.MaximumHistoryRows | Should -Be $minimumJobHistoryRows -Because "you choose to have it disabled."
+}
+
+function Assert-JobHistoryRows {
+    param (
+        $AgentServer,
+        $minimumJobHistoryRows
+    )
+    $AgentServer.MaximumHistoryRows | Should -BeGreaterOrEqual $minimumJobHistoryRows -Because "It should be enough to keep a certain amount of history entries."
+}
+
+function Assert-JobHistoryRowsPerJob {
+    param (
+        $AgentServer,
+        $minimumJobHistoryRowsPerJob
+    )
+    $AgentServer.MaximumJobHistoryRows | Should -BeGreaterOrEqual $minimumJobHistoryRowsPerJob -Because "It should be enough to keep a certain amount of history entries per job."
+}
