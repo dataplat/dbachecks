d9dded93706bbd1007f2dd30aff8f24d39ab06c9
Cláudio Silva <claudiosil100@gmail.com>
2018-11-30 23:26:49 +0000

    Added unit tests for job history

--- tests/checks/AgentChecks.Tests.ps1
+++ tests/checks/AgentChecks.Tests.ps1
@@ -29,4 +29,88 @@ Describe "Checking Agent.Tests.ps1 checks" -Tag UnitTest {
             Assert-MockCalled @assertMockParams
         }
     }
+
+    Context "Checking Job History" {
+        # if configured value for MaximumHistoryRows is -1 and test value -1 it will pass
+        It "Passes Check Correctly with Maximum History Rows disabled (-1)" {
+            # Mock to pass
+            Mock Get-DbaAgentServer {
+                [PSCustomObject]@{
+                    MaximumHistoryRows = -1;
+                    MaximumJobHistoryRows = 0;
+                }
+            }
+            $AgentServer = Get-DbaAgentServer -SqlInstance "Dummy" -EnableException:$false
+            Assert-JobHistoryRowsDisabled -AgentServer $AgentServer -minimumJobHistoryRows -1
+        }
+        # if configured value for MaximumHistoryRows is -1 and test value -1 it will pass
+        It "Fails Check Correctly with Maximum History Rows disabled (-1) but configured value is 1000" {
+            # Mock to fail
+            Mock Get-DbaAgentServer {
+                [PSCustomObject]@{
+                    MaximumHistoryRows = 1000;
+                    MaximumJobHistoryRows = 0;
+                }
+            }
+            $AgentServer = Get-DbaAgentServer -SqlInstance "Dummy" -EnableException:$false
+            { Assert-JobHistoryRowsDisabled -AgentServer $AgentServer -minimumJobHistoryRows -1 } | Should -Throw -ExpectedMessage "Expected '-1', because Maximum job history configuration should be disabled, but got 1000."
+        }
+        # if configured value for MaximumHistoryRows is 10000 and test value 10000 it will pass
+        It "Passes Check Correctly with Maximum History Rows being 10000" {
+            # Mock to pass
+            Mock Get-DbaAgentServer {
+                [PSCustomObject]@{
+                    MaximumHistoryRows = 10000;
+                    MaximumJobHistoryRows = 0;
+                }
+            }
+            $AgentServer = Get-DbaAgentServer -SqlInstance "Dummy" -EnableException:$false
+            Assert-JobHistoryRows -AgentServer $AgentServer -minimumJobHistoryRows 10000
+        }
+        # if configured value for MaximumHistoryRows is -1 and test value -1 it will pass
+        It "Fails Check Correctly with Maximum History Rows being less than 10000" {
+            # Mock to fail
+            Mock Get-DbaAgentServer {
+                [PSCustomObject]@{
+                    MaximumHistoryRows = 1000;
+                    MaximumJobHistoryRows = 0;
+                }
+            }
+            $AgentServer = Get-DbaAgentServer -SqlInstance "Dummy" -EnableException:$false
+            { Assert-JobHistoryRows -AgentServer $AgentServer -minimumJobHistoryRows 10000 } | Should -Throw -ExpectedMessage "Expected the actual value to be greater than or equal to 10000, because We expect the maximum job history row configuration to be greater than the configured setting 10000, but got 1000."
+        }
+        # if configured value is 100 and test value 100 it will pass
+        It "Passes Check Correctly with Maximum History Rows per job being 100" {
+            # Mock to pass
+            Mock Get-DbaAgentServer {
+                [PSCustomObject]@{
+                    MaximumHistoryRows = 1000;
+                    MaximumJobHistoryRows = 100;
+                }
+            }
+            $AgentServer = Get-DbaAgentServer -SqlInstance "Dummy" -EnableException:$false
+            Assert-JobHistoryRowsPerJob -AgentServer $AgentServer -minimumJobHistoryRowsPerJob 100
+        }
+        # if configured value is -1 and test value -1 it will pass
+        It "Fails Check Correctly with Maximum History Rows per job being less than 100" {
+            # Mock to fail
+            Mock Get-DbaAgentServer {
+                [PSCustomObject]@{
+                    MaximumHistoryRows = 100;
+                    MaximumJobHistoryRows = 50;
+                }
+            }
+            $AgentServer = Get-DbaAgentServer -SqlInstance "Dummy" -EnableException:$false
+            { Assert-JobHistoryRowsPerJob -AgentServer $AgentServer -minimumJobHistoryRowsPerJob 100 } | Should -Throw -ExpectedMessage "Expected the actual value to be greater than or equal to 100, because We expect the maximum job history row configuration per agent job to be greater than the configured setting 100, but got 50."
+        }
+        # Validate we have called the mock the correct number of times
+        It "Should call the mocks" {
+            $assertMockParams = @{
+                'CommandName' = 'Get-DbaAgentServer'
+                'Times'       = 6
+                'Exactly'     = $true
+            }
+            Assert-MockCalled @assertMockParams
+        }
+    }
 }
