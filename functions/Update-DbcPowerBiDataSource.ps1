3953b851c3f43c6c593437c728a9a41ca3add22f
SQLDBAWithABeard <mrrobsewell@outlook.com>
2019-07-08 16:33:02 +0100

    Fiixed error with Begin blocks

--- functions/Update-DbcPowerBiDataSource.ps1
+++ functions/Update-DbcPowerBiDataSource.ps1
@@ -112,156 +112,86 @@ function Update-DbcPowerBiDataSource {
         [switch]$EnableException,
         [switch]$Append
     )
-    if ($IsLinux) {
-        Write-PSFMessage "We cannot run this command from linux at the moment" -Level Warning
-        Return
-    }
-    else {
-        begin {
-            if ($FileName -ne "Default") {
-                if ($PSCmdlet.ShouldProcess($Path, 'Removing .json files named -like *Default*')) {
-                    $null = Remove-Item "$Path\*Default*.json" -ErrorAction SilentlyContinue
-                }
-            }
-            if ($Force) {
-                if ($PSCmdlet.ShouldProcess($Path, 'Removing all .json files')) {
-                    $null = Remove-Item "$Path\*.json" -ErrorAction SilentlyContinue
-                }
-            }
-            if ($IsLinux -and $Path -eq '\temp\dbachecks') {
-                $Path = Get-DbcConfigValue -Name app.localapp
-            }
+    begin {
+        Write-PSFMessage "Starting begin block" -Level Debug
+        if ($IsLinux -and $Path -eq '\temp\dbachecks') {
+            Write-PSFMessage "Setting path on Linux" -Level Verbose
+            $Path = Get-DbcConfigValue -Name app.localapp
+            Write-PSFMessage "Setting path on Linux to $Path" -Level Verbose
         }
-        process {
-            ++$i
-            try {
-                if (-not (Test-Path -Path $Path)) {
-                    if ($PSCmdlet.ShouldProcess($Path, 'Creating new directory')) {
-                        $null = New-Item -ItemType Directory -Path $Path -ErrorAction Stop
-                    }
-                }
-            }
-            catch {
-                Stop-PSFFunction -Message "Failure" -ErrorRecord $_
-                return
-            }
-            $basename = "dbachecks_$i"
-            if ($Environment) {
-                $basename = "dbachecks_$i" + "_$Environment`_"
-            }
-            if ($FileName) {
-                $basename = $FileName
-            }
-            else {
-                if ($InputObject.TagFilter) {
-                    $tagnames = $InputObject.TagFilter[0..3] -join "_"
-                    $basename = "$basename`_" + $tagnames + ".json"
-                }
-            }  
-
-            if ($basename.EndsWith('.json')) {}
-            else {
-                $basename = $basename + ".json"
-            }
-
-            $FilePath = "$Path\$basename"
-
-            if ($InputObject.TotalCount -gt 0) {
+        if ($Force) {
+            if ($PSCmdlet.ShouldProcess($Path, 'Removing all .json files')) {
                 try {
-                    if ($PSCmdlet.ShouldProcess($FilePath, 'Passing results')) {
-                        if ($Append) {
-                            $InputObject.TestResult | ConvertTo-Json -Depth 3 | Out-File -FilePath $FilePath -Append
-                            Write-PSFMessage -Level Output -Message "Appended results to $FilePath"
-                        }
-                        else {
-                            $InputObject.TestResult | ConvertTo-Json -Depth 3 | Out-File -FilePath $FilePath
-                            Write-PSFMessage -Level Output -Message "Wrote results to $FilePath"
-                        }
-                    }
+                    $null = Remove-Item "$Path\*.json" -ErrorAction Stop
+                    Write-PSFMessage "Removed all files from $Path" -Level Verbose
                 }
                 catch {
-                    Stop-PSFFunction -Message "Failure" -ErrorRecord $_
+                    Stop-PSFFunction -Message "FAILED - Removing all files from $Path" -ErrorRecord $_
                     return
                 }
             }
         }
-        end {
-            if ($InputObject.TotalCount -isnot [int]) {
-                Stop-PSFFunction -Message "Invalid TestResult. Did you forget to use -Passthru with Invoke-DbcCheck?"
+    }
+    process {
+        ++$i
+        try {
+            if (-not (Test-Path -Path $Path)) {
+                if ($PSCmdlet.ShouldProcess($Path, 'Creating new directory')) {
+                    $null = New-Item -ItemType Directory -Path $Path -ErrorAction Stop
+                    Write-PSFMessage "Created New Directory $Path" -Level Verbose
+                }
+            }
+        }
+        catch {
+            Stop-PSFFunction -Message "Failed - Creating New Directory $Path" -ErrorRecord $_
+            return
+        }
+        $basename = "dbachecks_$i"
+        if ($Environment) {
+            $basename = "dbachecks_$i" + "_$Environment`_"
+        }
+        if ($FileName) {
+            $basename = $FileName
+        }
+        else {
+            if ($InputObject.TagFilter) {
+                $tagnames = $InputObject.TagFilter[0..3] -join "_"
+                $basename = "$basename`_" + $tagnames + ".json"
+            }
+        }  
+
+        if ($basename.EndsWith('.json')) {}
+        else {
+            $basename = $basename + ".json"
+        }
+
+        Write-PSFMessage "Set basename to $basename" -Level Verbose
+        $FilePath = "$Path\$basename"
+        Write-PSFMessage "Set filepath to $FilePath" -Level Verbose
+
+        if ($InputObject.TotalCount -gt 0) {
+            try {
+                if ($PSCmdlet.ShouldProcess($FilePath, 'Passing results')) {
+                    if ($Append) {
+                        $InputObject.TestResult | ConvertTo-Json -Depth 3 | Out-File -FilePath $FilePath -Append
+                        Write-PSFMessage -Level Output -Message "Appended results to $FilePath"
+                    }
+                    else {
+                        $InputObject.TestResult | ConvertTo-Json -Depth 3 | Out-File -FilePath $FilePath
+                        Write-PSFMessage -Level Output -Message "Wrote results to $FilePath"
+                    }
+                }
+            }
+            catch {
+                Stop-PSFFunction -Message "Failed Passing Results to $FilePath" -ErrorRecord $_
                 return
             }
         }
     }
+    end {
+        if ($InputObject.TotalCount -isnot [int]) {
+            Stop-PSFFunction -Message "Invalid TestResult. Did you forget to use -Passthru with Invoke-DbcCheck?"
+            return
+        }
+    }
 }
-# SIG # Begin signature block
-# MIINEAYJKoZIhvcNAQcCoIINATCCDP0CAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
-# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQURdj1AZKEnhqebBxoCQexsea1
-# /7igggpSMIIFGjCCBAKgAwIBAgIQAsF1KHTVwoQxhSrYoGRpyjANBgkqhkiG9w0B
-# AQsFADByMQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYD
-# VQQLExB3d3cuZGlnaWNlcnQuY29tMTEwLwYDVQQDEyhEaWdpQ2VydCBTSEEyIEFz
-# c3VyZWQgSUQgQ29kZSBTaWduaW5nIENBMB4XDTE3MDUwOTAwMDAwMFoXDTIwMDUx
-# MzEyMDAwMFowVzELMAkGA1UEBhMCVVMxETAPBgNVBAgTCFZpcmdpbmlhMQ8wDQYD
-# VQQHEwZWaWVubmExETAPBgNVBAoTCGRiYXRvb2xzMREwDwYDVQQDEwhkYmF0b29s
-# czCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAI8ng7JxnekL0AO4qQgt
-# Kr6p3q3SNOPh+SUZH+SyY8EA2I3wR7BMoT7rnZNolTwGjUXn7bRC6vISWg16N202
-# 1RBWdTGW2rVPBVLF4HA46jle4hcpEVquXdj3yGYa99ko1w2FOWzLjKvtLqj4tzOh
-# K7wa/Gbmv0Si/FU6oOmctzYMI0QXtEG7lR1HsJT5kywwmgcjyuiN28iBIhT6man0
-# Ib6xKDv40PblKq5c9AFVldXUGVeBJbLhcEAA1nSPSLGdc7j4J2SulGISYY7ocuX3
-# tkv01te72Mv2KkqqpfkLEAQjXgtM0hlgwuc8/A4if+I0YtboCMkVQuwBpbR9/6ys
-# Z+sCAwEAAaOCAcUwggHBMB8GA1UdIwQYMBaAFFrEuXsqCqOl6nEDwGD5LfZldQ5Y
-# MB0GA1UdDgQWBBRcxSkFqeA3vvHU0aq2mVpFRSOdmjAOBgNVHQ8BAf8EBAMCB4Aw
-# EwYDVR0lBAwwCgYIKwYBBQUHAwMwdwYDVR0fBHAwbjA1oDOgMYYvaHR0cDovL2Ny
-# bDMuZGlnaWNlcnQuY29tL3NoYTItYXNzdXJlZC1jcy1nMS5jcmwwNaAzoDGGL2h0
-# dHA6Ly9jcmw0LmRpZ2ljZXJ0LmNvbS9zaGEyLWFzc3VyZWQtY3MtZzEuY3JsMEwG
-# A1UdIARFMEMwNwYJYIZIAYb9bAMBMCowKAYIKwYBBQUHAgEWHGh0dHBzOi8vd3d3
-# LmRpZ2ljZXJ0LmNvbS9DUFMwCAYGZ4EMAQQBMIGEBggrBgEFBQcBAQR4MHYwJAYI
-# KwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmRpZ2ljZXJ0LmNvbTBOBggrBgEFBQcwAoZC
-# aHR0cDovL2NhY2VydHMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0U0hBMkFzc3VyZWRJ
-# RENvZGVTaWduaW5nQ0EuY3J0MAwGA1UdEwEB/wQCMAAwDQYJKoZIhvcNAQELBQAD
-# ggEBANuBGTbzCRhgG0Th09J0m/qDqohWMx6ZOFKhMoKl8f/l6IwyDrkG48JBkWOA
-# QYXNAzvp3Ro7aGCNJKRAOcIjNKYef/PFRfFQvMe07nQIj78G8x0q44ZpOVCp9uVj
-# sLmIvsmF1dcYhOWs9BOG/Zp9augJUtlYpo4JW+iuZHCqjhKzIc74rEEiZd0hSm8M
-# asshvBUSB9e8do/7RhaKezvlciDaFBQvg5s0fICsEhULBRhoyVOiUKUcemprPiTD
-# xh3buBLuN0bBayjWmOMlkG1Z6i8DUvWlPGz9jiBT3ONBqxXfghXLL6n8PhfppBhn
-# daPQO8+SqF5rqrlyBPmRRaTz2GQwggUwMIIEGKADAgECAhAECRgbX9W7ZnVTQ7Vv
-# lVAIMA0GCSqGSIb3DQEBCwUAMGUxCzAJBgNVBAYTAlVTMRUwEwYDVQQKEwxEaWdp
-# Q2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5jb20xJDAiBgNVBAMTG0Rp
-# Z2lDZXJ0IEFzc3VyZWQgSUQgUm9vdCBDQTAeFw0xMzEwMjIxMjAwMDBaFw0yODEw
-# MjIxMjAwMDBaMHIxCzAJBgNVBAYTAlVTMRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMx
-# GTAXBgNVBAsTEHd3dy5kaWdpY2VydC5jb20xMTAvBgNVBAMTKERpZ2lDZXJ0IFNI
-# QTIgQXNzdXJlZCBJRCBDb2RlIFNpZ25pbmcgQ0EwggEiMA0GCSqGSIb3DQEBAQUA
-# A4IBDwAwggEKAoIBAQD407Mcfw4Rr2d3B9MLMUkZz9D7RZmxOttE9X/lqJ3bMtdx
-# 6nadBS63j/qSQ8Cl+YnUNxnXtqrwnIal2CWsDnkoOn7p0WfTxvspJ8fTeyOU5JEj
-# lpB3gvmhhCNmElQzUHSxKCa7JGnCwlLyFGeKiUXULaGj6YgsIJWuHEqHCN8M9eJN
-# YBi+qsSyrnAxZjNxPqxwoqvOf+l8y5Kh5TsxHM/q8grkV7tKtel05iv+bMt+dDk2
-# DZDv5LVOpKnqagqrhPOsZ061xPeM0SAlI+sIZD5SlsHyDxL0xY4PwaLoLFH3c7y9
-# hbFig3NBggfkOItqcyDQD2RzPJ6fpjOp/RnfJZPRAgMBAAGjggHNMIIByTASBgNV
-# HRMBAf8ECDAGAQH/AgEAMA4GA1UdDwEB/wQEAwIBhjATBgNVHSUEDDAKBggrBgEF
-# BQcDAzB5BggrBgEFBQcBAQRtMGswJAYIKwYBBQUHMAGGGGh0dHA6Ly9vY3NwLmRp
-# Z2ljZXJ0LmNvbTBDBggrBgEFBQcwAoY3aHR0cDovL2NhY2VydHMuZGlnaWNlcnQu
-# Y29tL0RpZ2lDZXJ0QXNzdXJlZElEUm9vdENBLmNydDCBgQYDVR0fBHoweDA6oDig
-# NoY0aHR0cDovL2NybDQuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0QXNzdXJlZElEUm9v
-# dENBLmNybDA6oDigNoY0aHR0cDovL2NybDMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0
-# QXNzdXJlZElEUm9vdENBLmNybDBPBgNVHSAESDBGMDgGCmCGSAGG/WwAAgQwKjAo
-# BggrBgEFBQcCARYcaHR0cHM6Ly93d3cuZGlnaWNlcnQuY29tL0NQUzAKBghghkgB
-# hv1sAzAdBgNVHQ4EFgQUWsS5eyoKo6XqcQPAYPkt9mV1DlgwHwYDVR0jBBgwFoAU
-# Reuir/SSy4IxLVGLp6chnfNtyA8wDQYJKoZIhvcNAQELBQADggEBAD7sDVoks/Mi
-# 0RXILHwlKXaoHV0cLToaxO8wYdd+C2D9wz0PxK+L/e8q3yBVN7Dh9tGSdQ9RtG6l
-# jlriXiSBThCk7j9xjmMOE0ut119EefM2FAaK95xGTlz/kLEbBw6RFfu6r7VRwo0k
-# riTGxycqoSkoGjpxKAI8LpGjwCUR4pwUR6F6aGivm6dcIFzZcbEMj7uo+MUSaJ/P
-# QMtARKUT8OZkDCUIQjKyNookAv4vcn4c10lFluhZHen6dGRrsutmQ9qzsIzV6Q3d
-# 9gEgzpkxYz0IGhizgZtPxpMQBvwHgfqL2vmCSfdibqFT+hKUGIUukpHqaGxEMrJm
-# oecYpJpkUe8xggIoMIICJAIBATCBhjByMQswCQYDVQQGEwJVUzEVMBMGA1UEChMM
-# RGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3d3cuZGlnaWNlcnQuY29tMTEwLwYDVQQD
-# EyhEaWdpQ2VydCBTSEEyIEFzc3VyZWQgSUQgQ29kZSBTaWduaW5nIENBAhACwXUo
-# dNXChDGFKtigZGnKMAkGBSsOAwIaBQCgeDAYBgorBgEEAYI3AgEMMQowCKACgACh
-# AoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwGCisGAQQBgjcCAQsxDjAM
-# BgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBTD/A01VkOZ5BM6p+hOZZpdlPWC
-# JTANBgkqhkiG9w0BAQEFAASCAQAk0pEcVfWA9swDFvMQbK0/iFCgBnLpx2j6Tu3y
-# h6zlzVrNQvLqdRNgj1Rm2SMCC/QrV29FGP+2/px5iC+LpzfMuxW+8vY1652UsLdi
-# yAMjY09WQe0e1QgUVYsDF8paaZVDe8IeumF0Dm9qd0jZ2j6vWfKDxtjKshRHUwfx
-# lRXQxyNgpSy3srA6QkBR4Um5b+lSoQqQ5egcFJbpUexkXSlmzNU8+NXGbcF0P8M8
-# SiunK+iCDOl2Na2DE3ncHH3Atzi95f/yekc6Lvx/koORUElihYqcCtOUVD40vqJ+
-# ay4QCLA/BE0KOUMo9XERaKMOI1iqh4vhHuLxR1UEEcfhHEPg
-# SIG # End signature block
