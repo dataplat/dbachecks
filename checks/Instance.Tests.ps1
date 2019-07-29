d2101a0806eea6dcc3224fa1e1da9fec8a056714
SQLDBAWithABeard <mrrobsewell@outlook.com>
2019-07-29 12:33:04 +0100

    added default trace check #672

--- checks/Instance.Tests.ps1
+++ checks/Instance.Tests.ps1
@@ -795,7 +795,23 @@ $Tags = Get-CheckInformation -Check $Check -Group Instance -AllChecks $AllChecks
             }
         }
     }
-
+    Describe "Default Trace" -Tags DefaultTrace, Low, $filename {
+        $skip = Get-DbcConfigValue skip.instance.defaulttrace
+        if ($NotContactable -contains $psitem) {
+            Context "Checking Default Trace on $psitem" {
+                It "Can't Connect to $Psitem" -Skip:$skip {
+                    $false	|  Should -BeTrue -Because "The instance should be available to be connected to!"
+                }
+            }
+        }
+        else {
+            Context "Checking Default Trace on $psitem" -Skip:$skip {
+                It "The Default Trace should be enabled on $psitem" {
+                    Assert-DefaultTrace -AllInstanceInfo $AllInstanceInfo
+                }
+            }
+        }
+    }
 }
 
 Describe "SQL Browser Service" -Tags SqlBrowserServiceAccount, ServiceAccount, High, $filename {
