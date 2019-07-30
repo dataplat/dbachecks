6313fbd1054566dc0c9283e646af11fccaf8c8c2
SQLDBAWithABeard <mrrobsewell@outlook.com>
2019-07-30 15:18:02 +0100

    testing last run duration #239

--- checks/Agent.Tests.ps1
+++ checks/Agent.Tests.ps1
@@ -340,6 +340,73 @@ Set-PSFConfig -Module dbachecks -Name global.notcontactable -Value $NotContactab
                         }
                     }   
                 }
+                Describe "Last Agent Job Run" -Tags LastJobRunTime, $filename {
+                    $skip = Get-DbcConfigValue skip.agent.lastjobruntime
+                    $runningjobpercentage = Get-DbcConfigValue agent.lastjobruntime.percentage
+                    if (-not $skip) {
+                        $query = "IF OBJECT_ID('tempdb..#dbachecksLastRunTime') IS NOT NULL DROP Table #dbachecksLastRunTime
+                        SELECT * INTO #dbachecksLastRunTime
+                        FROM
+                        (
+                        SELECT
+                        j.job_id,
+                        j.name AS JobName,
+                        jh.run_duration AS Duration
+                        FROM msdb.dbo.sysjobs j 
+                        INNER JOIN
+                            (
+                                SELECT job_id, instance_id = MAX(instance_id)
+                                    FROM msdb.dbo.sysjobhistory
+                                    GROUP BY job_id
+                            ) AS h
+                            ON j.job_id = h.job_id
+                        INNER JOIN
+                            msdb.dbo.sysjobhistory AS jh
+                            ON jh.job_id = h.job_id
+                            AND jh.instance_id = h.instance_id
+                        ) AS lrt
+                        
+                        IF OBJECT_ID('tempdb..#dbachecksAverageRunTime') IS NOT NULL DROP Table #dbachecksAverageRunTime
+                        SELECT * INTO #dbachecksAverageRunTime
+                        FROM
+                        (
+                        SELECT 
+                        job_id,
+                        AVG(DATEDIFF(SECOND, 0, STUFF(STUFF(RIGHT('000000' + CONVERT(VARCHAR(6),run_duration),6),5,0,':'),3,0,':'))) AS AvgSec
+                        FROM msdb.dbo.sysjobhistory hist
+                        GROUP BY job_id
+                        ) as art
+                        
+                        SELECT 
+                        JobName,
+                        Duration,
+                        AvgSec,
+                        Duration - AvgSec AS Diff
+                        FROM #dbachecksLastRunTime lastrun
+                        JOIN #dbachecksAverageRunTime avgrun
+                        ON lastrun.job_id = avgrun.job_id
+                        
+                        DROP Table #dbachecksLastRunTime
+                        DROP Table #dbachecksAverageRunTime"
+                        $lastagentjobruns = Invoke-DbaQuery -SqlInstance $PSItem -Database msdb -Query $query
+                    }
+                    if ($NotContactable -contains $psitem) {
+                        Context "Testing last job run time on $psitem" {
+                            It "Can't Connect to $Psitem" {
+                                $false  |  Should -BeTrue -Because "The instance should be available to be connected to!"
+                            }
+                        }
+                    }
+                    else {    
+                        Context "Testing last job run time on $psitem" {
+                            foreach ($lastagentjobrun in $lastagentjobruns) {
+                                It "Job $($lastagentjobrun.JobName) last run duration should be less than $runningjobpercentage % of average run time on $psitem" -Skip:$skip {
+                                    Assert-LastJobRun -lastagentjobrun $lastagentjobrun -runningjobpercentage $runningjobpercentage
+                                }
+                            }
+                        }
+                    }   
+                }
             }
         }
     }
