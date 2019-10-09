f05324e9cf9ee6581c0c6f68936b218c6ab30397
Gareth Newman (Admin) <it.gareth.newman@PMGROUP.LOCAL>
2019-10-09 15:02:31 +0100

    beard comments

--- checks/Instance.Tests.ps1
+++ checks/Instance.Tests.ps1
@@ -196,9 +196,9 @@ $Tags = Get-CheckInformation -Check $Check -Group Instance -AllChecks $AllChecks
         }
     }
 
-    Describe "Default File Paths" -Tags DefaultFilePaths, $filename {
+    Describe "Default File Path" -Tags DefaultFilePath, $filename {
         if ($NotContactable -contains $psitem) {
-            Context "Testing Default File Paths on $psitem" {
+            Context "Testing Default File Path on $psitem" {
                 It "Can't Connect to $Psitem" {
                     $false	|  Should -BeTrue -Because "The instance should be available to be connected to!"
                 }
@@ -206,13 +206,13 @@ $Tags = Get-CheckInformation -Check $Check -Group Instance -AllChecks $AllChecks
         }
         else {
             Context "Testing Default Data File Path on $psitem" {
-                It "Default Data File Paths on $psitem" {
+                It "Default Data File Path on $psitem" {
                     $diskFile = Get-DbaInstanceProperty -SqlInstance $psitem | Where-Object Name -eq DefaultFile
                     $diskFile.Value.substring(0,1) | Should -Not -Be "C" -Because 'Default Data file path should not be your C:\ drive'
                 }
             }
             Context "Testing Default Log File Path on $psitem" {
-                It "Default Log File Paths on $psitem" {
+                It "Default Log File Path on $psitem" {
                     $diskLog = Get-DbaInstanceProperty -SqlInstance $psitem | Where-Object Name -eq DefaultLog
                     $diskLog.Value.substring(0,1) | Should -Not -Be "C" -Because 'Dafault Log file path should not be your C:\ drive'
                 }
