3e92afa17cf788b485e40c34d09d4e85059c503d
Rob.Sewell <Rob.Sewell@Jet2.com>
2019-11-26 17:36:00 +0000

    Need to return all of the tags for a none-unique tag

--- internal/functions/Get-CheckInformation.ps1
+++ internal/functions/Get-CheckInformation.ps1
@@ -3,7 +3,7 @@ function Get-CheckInformation {
     Param($Group, $Check, $AllChecks ,$ExcludeCheck)
     ## need to reset the variable here
     $script:localapp = Get-DbcConfigValue -Name app.localapp
-    $GroupChecksConfig = (Get-DbcCheck).Where{$_.Group -eq $Group}
+    $GroupChecksConfig = Get-DbcCheck -Group $Group
     # Nothing if we exclude the group
     if ($ExcludeCheck -contains $Group) {Return}
     # Create an array of tags for the group except the Group Name
@@ -42,7 +42,7 @@ function Get-CheckInformation {
         @($Check).ForEach{
             if($GroupChecks -contains $psitem){
     ## BUT - This falls flat when you use a tag for a number of Checks that is not a group (like CIS) in that case all you get in $CheckInfo is CIS and not the relevant unique tags
-                (Get-DbcCheck $psitem).ForEach{
+                @(Get-DbcCheck -Pattern $psitem).ForEach{
                     $CheckInfo += $psitem.UniqueTag
                 } 
             }
