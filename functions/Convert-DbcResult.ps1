b5f34326d8c0a13be004b61f5673278aed409280
Rob <Rob@TheBeard.local>
2019-12-28 14:16:21 +0000

    Added help

--- functions/Convert-DbcResult.ps1
+++ functions/Convert-DbcResult.ps1
@@ -1,3 +1,38 @@
+<#
+.SYNOPSIS
+Takes the results of Invoke-DbcCheck, parses it and converts it to a datatable object
+
+.DESCRIPTION
+You need to run Invoke-DbcCheck with the PassThru parameter and this command will take the
+results and parse them creating a datatable object with column headings 
+Date Label Describe Context Name Database ComputerName Instance Result FailureMessage
+so that it can be written to a database with Write-DbcTable (or Write-DbaDataTable) or to
+a file with Set-DbcFile
+
+.PARAMETER TestResults
+The output of Invoke-DbcCheck (WITH -PassThru)
+
+.PARAMETER Label
+An optional label to add to the set of results to identify them - Think Morning-Checks or New-instance
+
+.EXAMPLE
+Invoke-DbcCheck -SqlInstance SQL2017N5 -Check AutoClose -Passthru | Convert-DbcResult -Label Beard-Check
+
+Runs the AutoClose check against SQL2017N5 and converts to a datatable with a label of Beard-Check
+
+.EXAMPLE
+Invoke-DbcCheck -SqlInstance SQL2017N5 -Check AutoClose -Passthru | Convert-DbcResult -Label Beard-Check | Write-DbcTable -SqlInstance sql2017n5 -Database tempdb -Table newdbachecks  
+
+Runs the AutoClose check against SQL2017N5 and converts to a datatable with a label of Beard-Check and writes it to a table newdbachecks in tempdb on SQL2017N5 (NB Don't use tempdb!!)
+
+.EXAMPLE
+Invoke-DbcCheck -SqlInstance SQL2017N5 -Check AutoClose -Passthru | Convert-DbcResult -Label Beard-Check | Set-DbcFile -FilePath C:\temp\dbachecks\ -FileName Auto-close.json -FileType Json
+
+Runs the AutoClose check against SQL2017N5 and converts to a datatable with a label of Beard-Check and outputs to JSON and saves in C:\temp\dbachecks\Auto-close.json 
+
+.NOTES
+Intial - RMS 28/12/2019
+#>
 function Convert-DbcResult {
     [OutputType([System.Data.DataTable])]
     Param(
