6d6ba69e4f5dd96647fc10c743627df727e8ebec
Rob <Rob@TheBeard.local>
2019-12-28 14:17:20 +0000

    added help

--- functions/Set-DbcFile.ps1
+++ functions/Set-DbcFile.ps1
@@ -1,3 +1,63 @@
+<#
+.SYNOPSIS
+Writes the result of Invoke-DbcCheck to a file (after converting with Convert-DbcResult)
+
+.DESCRIPTION
+When a check has been run with Invoke-DbcCheck (and -PassThru) and then converted with
+Convert-DbcResult This command will write the results to a CSV, JSON or XML file
+
+.PARAMETER InputObject
+The datatable created by Convert-DbcResult
+
+.PARAMETER FilePath
+Thedirectory for the file
+
+.PARAMETER FileName
+Thename of the file
+
+.PARAMETER FileType
+Thetype of file -CSV,JSON,XML
+
+.PARAMETER Append
+Add to an existing file or not
+
+.PARAMETER Force
+Overwrite Existing file
+
+.EXAMPLE
+$Date = Get-Date -Format "yyyy-MM-dd"
+Invoke-DbcCheck -SqlInstance SQL2017N5 -Check AutoClose -Passthru | Convert-DbcResult -Label Beard-Check | Set-DbcFile -FilePath C:\temp\dbachecks\ -FileName Auto-close_$Date -FileType xml
+
+Runs the AutoClose check against SQL2017N5 and converts to a datatable with a label of Beard-Check and outputs to xml and saves in C:\temp\dbachecks\Auto-close_DATE.xml 
+
+.EXAMPLE
+Invoke-DbcCheck -SqlInstance SQL2017N5 -Check AutoClose -Passthru | Convert-DbcResult -Label Beard-Check | Set-DbcFile -FilePath C:\temp\dbachecks\ -FileName Auto-close.xml -FileType xml
+
+Runs the AutoClose check against SQL2017N5 and converts to a datatable with a label of Beard-Check and outputs to xml and saves in C:\temp\dbachecks\Auto-close.xml 
+
+.EXAMPLE
+Invoke-DbcCheck -SqlInstance SQL2017N5 -Check AutoClose -Passthru | Convert-DbcResult -Label Beard-Check | Set-DbcFile -FilePath C:\temp\dbachecks\ -FileName Auto-close.csv -FileType csv
+
+Runs the AutoClose check against SQL2017N5 and converts to a datatable with a label of Beard-Check and outputs to csv and saves in C:\temp\dbachecks\Auto-close.csv 
+
+.EXAMPLE
+Invoke-DbcCheck -SqlInstance SQL2017N5 -Check AutoClose -Passthru | Convert-DbcResult -Label Beard-Check | Set-DbcFile -FilePath C:\temp\dbachecks\ -FileName Auto-close.json -FileType Json
+
+Runs the AutoClose check against SQL2017N5 and converts to a datatable with a label of Beard-Check and outputs to JSON and saves in C:\temp\dbachecks\Auto-close.json 
+
+.EXAMPLE
+Invoke-DbcCheck -SqlInstance SQL2017N5 -Check AutoClose -Passthru | Convert-DbcResult -Label Beard-Check | Set-DbcFile -FilePath C:\temp\dbachecks\ -FileName Auto-close.json -FileType Json -Append
+
+Runs the AutoClose check against SQL2017N5 and converts to a datatable with a label of Beard-Check and outputs to JSON and saves in C:\temp\dbachecks\Auto-close.json appending the results to the exisitng file
+
+.EXAMPLE
+Invoke-DbcCheck -SqlInstance SQL2017N5 -Check AutoClose -Passthru | Convert-DbcResult -Label Beard-Check | Set-DbcFile -FilePath C:\temp\dbachecks\ -FileName Auto-close.json -FileType Json -Forec
+
+Runs the AutoClose check against SQL2017N5 and converts to a datatable with a label of Beard-Check and outputs to JSON and saves in C:\temp\dbachecks\Auto-close.json overwriting the exisitng file
+
+.NOTES
+Intial - RMS 28/12/2019
+#>
 function Set-DbcFile {
     [CmdletBinding(SupportsShouldProcess, DefaultParametersetName = "Default")]
     Param(
