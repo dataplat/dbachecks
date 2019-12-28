cdcaeb1ba18a066210cba8b6dd777e1bd212d994
Rob <Rob@TheBeard.local>
2019-12-28 14:16:54 +0000

    added help

--- functions/Write-DbcTable.ps1
+++ functions/Write-DbcTable.ps1
@@ -1,3 +1,41 @@
+<#
+.SYNOPSIS
+Writes the result of Invoke-DbcCheck (with -PassThru) after Convert-DbcResult to a database table
+
+.DESCRIPTION
+After running Invoke-DbcCheck (With PassThru) and converting it to a datatable with Convert-DbcResult, this command
+will writet the results to a database table and will also write the current Checks to another table called dbachecksChecks
+
+.PARAMETER SqlInstance
+The Instance for the results
+
+.PARAMETER SqlCredential
+The SQL Credential for the instance if required
+
+.PARAMETER Database
+The database to write the results
+
+.PARAMETER InputObject
+The datatable from Convert-DbcResult
+
+.PARAMETER Table
+The name of the table for the results - will be created if it doesnt exist
+
+.PARAMETER Schema
+the schema for the table - defaults to dbo
+
+.PARAMETER Truncate
+Will truncate the existing table (if results go to a staging table for example)
+
+
+.EXAMPLE
+Invoke-DbcCheck -SqlInstance SQL2017N5 -Check AutoClose -Passthru | Convert-DbcResult -Label Beard-Check | Write-DbcTable -SqlInstance sql2017n5 -Database tempdb -Table newdbachecks  
+
+Runs the AutoClose check against SQL2017N5 and converts to a datatable with a label of Beard-Check and writes it to a table newdbachecks in tempdb on SQL2017N5 (NB Don't use tempdb!!)
+
+.NOTES
+Intial - RMS 28/12/2019
+#>
 function Write-DbcTable {
     [CmdletBinding(SupportsShouldProcess)]
     Param(
