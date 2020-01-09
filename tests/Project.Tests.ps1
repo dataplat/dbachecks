3648bf3778a33c3bbdc582a3d040b9924d2e4ec1
Rob Sewell <mrrobsewell@outlook.com>
2020-01-09 16:43:12 +0000

    only check the files that have changed

--- tests/Project.Tests.ps1
+++ tests/Project.Tests.ps1
@@ -21,20 +21,24 @@ Describe "PSScriptAnalyzer rule-sets" -Tag Build , ScriptAnalyzer {
 
     $Rules = Get-ScriptAnalyzerRule
     $scripts = Get-ChildItem $ModuleBase -Include *.ps1, *.psm1, *.psd1 -Recurse | Where-Object fullname -notmatch 'classes'
+    # Get last commit that was merged from master
+    $lastCommit = git log --grep="Updated Version Number and docs from master" -1  --format='%H'
+    # Get the files that have been altered in since the last merge from master
+    $scripts= git diff --name-only $lastCommit HEAD | Where-Object {$psitem.EndsWith('ps1')}
 
     foreach ( $Script in $scripts )
     {
-        Context "Script '$($script.FullName)'" {
+        Context "Checking PSScriptAnalyzer on Script '$script'" {
 
             foreach ( $rule in $rules )
             {
                                 # Skip all rules that are on the exclusions list
                 if ($FunctionHelpTestExceptions -contains $rule.RuleName) { continue }
                 It "The Script Analyzer Rule [$rule] Should not fail" {
-                    $rulefailures = Invoke-ScriptAnalyzer -Path $script.FullName -IncludeRule $rule.RuleName -Settings $ModuleBase\PSScriptAnalyzerSettings.psd1
+                    $rulefailures = Invoke-ScriptAnalyzer -Path "$ModuleBase\$script" -IncludeRule $rule.RuleName -Settings $ModuleBase\PSScriptAnalyzerSettings.psd1
                     $message = ($rulefailures | Select-Object Message -Unique).Message
                     $lines = $rulefailures.Line -join ','
-                    $rulefailures.Count | Should -Be 0 -Because "Script Analyzer says the rules have been broken on lines $lines with Message '$message' Check in VSCode Problems tab or Run Invoke-ScriptAnalyzer -Script $($script.FullName) -Settings $ModuleBase\PSScriptAnalyzerSettings.psd1"
+                    $rulefailures.Count | Should -Be 0 -Because "Script Analyzer says the rules have been broken on lines $lines with Message '$message' Check in VSCode Problems tab or Run Invoke-ScriptAnalyzer -Script $ModuleBase\$script -Settings $ModuleBase\PSScriptAnalyzerSettings.psd1"
                 }
             }
         }
