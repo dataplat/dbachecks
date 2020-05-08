8d6560139b9b08268aaa34024a75f203a5d633b4
Rob <Rob@TheBeard.local>
2020-05-08 14:43:59 +0100

    Test names are long for index checks

--- functions/Write-DbcTable.ps1
+++ functions/Write-DbcTable.ps1
@@ -90,7 +90,7 @@ function Write-DbcTable {
      [Label] [nvarchar](255) NULL,
      [Describe] [nvarchar](255) NULL,
      [Context] [nvarchar](255) NULL,
-     [Name] [nvarchar](255) NULL,
+     [Name] [nvarchar](600) NULL,
      [Database] [nvarchar](255) NULL,
      [ComputerName] [nvarchar](255) NULL,
      [Instance] [nvarchar](255) NULL,
