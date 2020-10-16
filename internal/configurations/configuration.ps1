837ed3aba13f91ab78ade31e6cbff6bd76a86423
Ant Green <antg1985@hotmail.co.uk>
2020-10-16 14:43:51 +0100

    Add policy to exclude databases on the trustworthy check
    Recent implimentation of Azure Information Protection scanning resulted in a new database being created which Microsoft need to have trustworthy set to on, this threw out the error, so to not exlcude the DB from all checks, added new policy to exclude it from this one checl

--- internal/configurations/configuration.ps1
+++ internal/configurations/configuration.ps1
@@ -150,6 +150,7 @@ Set-PSFConfig -Module dbachecks -Name policy.database.status.excludeoffline -Val
 Set-PSFConfig -Module dbachecks -Name policy.database.status.excluderestoring -Value @() -Initialize -Description "Database names that we expect to be restoring"
 Set-PSFConfig -Module dbachecks -Name database.querystoreenabled.excludedb -Value @() -Initialize -Description "A List of databases that we do not want to check for Query Store enabled"
 Set-PSFConfig -Module dbachecks -Name database.querystoredisabled.excludedb -Value @() -Initialize -Description "A List of databases that we do not want to check for Query Store disabled"
+Set-PSFConfig -Module dbachecks -Name policy.database.trustworthyexcludedb -Value @() -Initialize -Description "A List of databases that we do not want to check for Trustworthy being on"
 
 # Policy for Ola Hallengren Maintenance Solution
 Set-PSFConfig -Module dbachecks -name policy.ola.installed -Validation bool -Value $true -Initialize -Description "Checks to see if Ola Hallengren solution is installed"
