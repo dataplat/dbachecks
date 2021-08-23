8a964b76932a65fe2bd2ed1e9d21d24c381e4a6a
SQLDbaWithABeard <mrrobsewell@outlook.com>
2021-08-23 18:20:50 +0100

    fixed scriptanalyser failures

--- functions/Export-DbcConfig.ps1
+++ functions/Export-DbcConfig.ps1
@@ -24,8 +24,8 @@ Using this switch turns this "nice by default" feature off and enables you to ca
 
 .EXAMPLE
     $config = Export-DbcConfig | Invoke-Item
-    
-    Exports config to "$script:localapp\config.json" as and opens it in a default application. 
+
+    Exports config to "$script:localapp\config.json" as and opens it in a default application.
 
 .LINK
 https://dbachecks.readthedocs.io/en/latest/functions/Export-DbcConfig/
@@ -33,16 +33,15 @@ https://dbachecks.readthedocs.io/en/latest/functions/Export-DbcConfig/
 function Export-DbcConfig {
     [CmdletBinding()]
     param (
-        [string]$Path = "$script:localapp\config.json",
-        [switch]$EnableException
+        [string]$Path = "$script:localapp\config.json"
     )
 
     try {
         Get-DbcConfig | Select-Object * | ConvertTo-Json -Depth 10 | Out-File -FilePath $Path -ErrorAction Stop
-        
+
         # support for Invoke-Item
-        Get-ChildItem -Path $Path
-        Write-PSFMessage -Message "Wrote file to $Path" -Level Verbose
+        Get-Item -Path $Path
+        Write-PSFMessage -Message "Wrote file to $Path" -Level Host
     }
     catch {
         Stop-PSFFunction -Message $_ -Target $Path
