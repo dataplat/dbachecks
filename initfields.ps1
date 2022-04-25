68bbf28c39ec3044ae2eaec7e095020bc915d936
SQLDbaWithABeard <mrrobsewell@outlook.com>
2022-04-25 10:37:27 +0100

    interesting results

--- initfields.ps1
+++ initfields.ps1
@@ -1,3 +1,118 @@
+# uses Jakubs Profiler Module - Install-Module Profiler
+$withinitFIeldsSMO = {
+    $password = ConvertTo-SecureString "dbatools.IO" -AsPlainText -Force
+    $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "sqladmin", $password
+    
+    $Sqlinstances = 'localhost,7401', 'localhost,7402', 'localhost,7403'
+    $smos = Connect-DbaInstance -SqlInstance $Sqlinstances -SqlCredential $cred 
+    
+    foreach ($smo in $smos) {
+        $smo.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Server], $false)
+        $smo.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Database], $false)
+        $smo.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Login], $false)
+        $smo.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Agent.Job], $false)
+        $initfields = $smo.GetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Server])
+        $initfields.Add("Collation")
+        $smo.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Server], $initfields)
+        $dbinitfields = $smo.GetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Database])
+        $dbinitfields.Add("Collation")
+        $smo.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Database], $initfields)
+    
+        [PSCustomObject]@{
+            ComputerName = $smo.ComputerName
+            InstanceName = $smo.DbaInstanceName
+            Collation    = $smo.Collation
+            Databases    = $Smo.Databases.ForEach{
+                [PSCustomObject]@{
+                    Name      = $_.Name
+                    Collation = $_.Collation
+                }
+            
+            }
+        }
+    }
+}
+
+$WithoutInitFieldsSMO = {
+    $password = ConvertTo-SecureString "dbatools.IO" -AsPlainText -Force
+    $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "sqladmin", $password
+    
+    $Sqlinstances = 'localhost,7401', 'localhost,7402', 'localhost,7403'
+    $smos = Connect-DbaInstance -SqlInstance $Sqlinstances -SqlCredential $cred
+    foreach ($smo in $smos) {
+        [PSCustomObject]@{
+            ComputerName = $smo.ComputerName
+            InstanceName = $smo.DbaInstanceName
+            Collation    = $smo.Collation
+            Databases    = $Smo.Databases.ForEach{
+                [PSCustomObject]@{
+                    Name      = $_.Name
+                    Collation = $_.Collation
+                }
+            
+            }
+        }
+    }
+}
+
+$justdbatools = {
+    $password = ConvertTo-SecureString "dbatools.IO" -AsPlainText -Force
+    $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "sqladmin", $password
+    
+    $Sqlinstances = 'localhost,7401', 'localhost,7402', 'localhost,7403'
+    $smos = Connect-DbaInstance -SqlInstance $Sqlinstances -SqlCredential $cred
+
+    Test-DbaDbCollation -SqlInstance $smos 
+}
+
+# so the initial load doesnt skew the figures
+ipmo dbatools
+
+
+$justdbatoolstrace = Trace-Script -ScriptBlock $justdbatools
+$WithoutInittrace = Trace-Script -ScriptBlock $WithoutInitFieldsSMO 
+$withinittrace = Trace-Script -ScriptBlock $withinitFIeldsSMO 
+
+
+$dbatoolsMessage = "With dbatools it takes {0} MilliSeconds" -f $justdbatoolstrace.StopwatchDuration.TotalMilliseconds
+$withinittMessage = "With initfields and SMO it takes {0} MilliSeconds" -f $withinittrace.StopwatchDuration.TotalMilliseconds
+$WithoutInitMessage = "Without initfields and SMO it takes {0} MilliSeconds" -f $WithoutInittrace.StopwatchDuration.TotalMilliseconds
+
+Write-PSFMessage -Message $dbatoolsMessage -Level Significant
+Write-PSFMessage -Message $WithoutInitMessage -Level Significant
+Write-PSFMessage -Message $withinittMessage -Level Significant
+
+<#
+pwsh > Write-PSFMessage -Message $dbatoolsMessage -Level Significant
+[10:36:20][<ScriptBlock>] With dbatools it takes 308.9962 MilliSeconds
+pwsh > Write-PSFMessage -Message $WithoutInitMessage -Level Significant
+[10:36:20][<ScriptBlock>] Without initfields and SMO it takes 270.5188 MilliSeconds
+pwsh > Write-PSFMessage -Message $withinittMessage -Level Significant
+[10:36:20][<ScriptBlock>] With initfields and SMO it takes 117.5935 MilliSeconds
+
+#>
+
+
+$10timesWithInit = Invoke-Script -ScriptBlock $withinitFIeldsSMO -Preheat 0 -Repeat 10 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # We run Pester V4 here because the -legacy parameter of Invoke-DbcCheck is set to true by default
 $password = ConvertTo-SecureString "dbatools.IO" -AsPlainText -Force
@@ -22,37 +137,3 @@ $initFieldsDb = New-Object System.Collections.Specialized.StringCollection
 $initFieldsLogin = New-Object System.Collections.Specialized.StringCollection
 $initFieldsJob = New-Object System.Collections.Specialized.StringCollection
 
-foreach ($smo in $smos[0]) {
-    $smo.SetDefaultInitFields($false)
-    $smo.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Database], $false)
-    $smo.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Login], $false)
-    $smo.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Agent.Job], $false)
-    $initfields = $smo.GetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Server])
-    $initfields.Add("BackupDirectory")
-    $smo.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Server], $initfields)
-
-    $smo | Fl
-}
-foreach ($smo in $smos[0]) {
-    $smo | Fl
-}
-$smo.GetDefaultInitFields($smo.Gettype())
-$smo.GetDefaultInitFields($smo.Columns.Gettype())
-$scriptBlock = { 
-    Import-Module Pester
-    Invoke-Pester
-}
-Trace-Script -ScriptBlock $scriptBlock
-
-
-.BackupDirectory
-$initfields = $server.GetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Server])
-$initfields.Add("BackupDirectory")
-$server.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Server], $initfields)
-
-
-
-
-
-$flag = @{ _profiler = $true }
-Invoke-Script -ScriptBlock $scriptBlock -Preheat 0 -Repeat 3 -Flag $flag
