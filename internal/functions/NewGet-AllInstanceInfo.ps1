fbbb81a988c520fe78dfe897213392e24568e02b
SQLDbaWithABeard <mrrobsewell@outlook.com>
2022-04-25 14:42:45 +0100

    new function to get all the info

---
+++ internal/functions/NewGet-AllInstanceInfo.ps1
@@ -0,0 +1,83 @@
+function NewGet-AllInstanceInfo {
+    # Using the unique tags gather the information required
+    Param($Instance, $Tags)
+    
+    #clear out the default initialised fields
+    $Instance.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Server], $false)
+    $Instance.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Database], $false)
+    $Instance.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Login], $false)
+    $Instance.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Agent.Job], $false)
+    $Instance.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.StoredProcedure], $false)
+    
+    # Server Initial fields
+    $ServerInitFields = $Instance.GetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Server])
+    $ServerInitFields.Add("VersionMajor") | Out-Null # so we can check versions
+    $Instance.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.Server], $ServerInitFields)
+
+    # Stored Procedure Initial Fields
+    $StoredProcedureInitFields = $Instance.GetDefaultInitFields([Microsoft.SqlServer.Management.Smo.StoredProcedure])
+    $StoredProcedureInitFields.Add("Startup") | Out-Null # So we can check SPs start up for the CIS checks
+
+    # Configuration cannot have default init fields :-)
+    $configurations = $false
+
+    # Set up blank ConfigValues object for any config we need to use in the checks
+    $ConfigValues = [PSCustomObject]@{}
+
+    # Using there so that if the instance is not contactable, no point carrying on with gathering more information
+    switch ($tags) {
+
+        'DefaultTrace' {
+            $configurations = $true
+        }
+        'OleAutomationProceduresDisabled' {
+            $configurations = $true
+        }
+        'CrossDBOwnershipChaining' {
+            $configurations = $true
+        }
+        'ScanForStartupProceduresDisabled' {
+            # we have to check the spconfigure and we have to check that any stored procedurees in master have startup set to true
+            $configurations = $true
+            $ScanForStartupProceduresDisabled = $true
+            $Instance.SetDefaultInitFields([Microsoft.SqlServer.Management.Smo.StoredProcedure], $StoredProcedureInitFields)
+            $ConfigValues | Add-Member -MemberType NoteProperty -Name 'scanforstartupproceduresdisabled' -Value (Get-DbcConfigValue policy.security.scanforstartupproceduresdisabled)
+        }
+        'RemoteAccessDisabled' {
+            $configurations = $true
+        }
+        'SQLMailXPsDisabled' {
+            $configurations = $true
+        }
+        'DAC' {
+            $configurations = $true
+            $ConfigValues | Add-Member -MemberType NoteProperty -Name 'dacallowed' -Value (Get-DbcConfigValue policy.dacallowed)
+        }
+        'OLEAutomation' {
+            $configurations = $true
+            $ConfigValues | Add-Member -MemberType NoteProperty -Name 'OLEAutomation' -Value (Get-DbcConfigValue policy.oleautomation)
+        }
+
+        Default { }
+    }
+
+    # set the default init fields for all the tags
+    
+    #build the object
+
+    $testInstanceObject = [PSCustomObject]@{
+        ComputerName  = $Instance.ComputerName
+        InstanceName  = $Instance.DbaInstanceName
+        Name          = $Instance.Name
+        ConfigValues  = $ConfigValues
+        VersionMajor  = $Instance.VersionMajor
+        Configuration = if ($configurations) { $Instance.Configuration }else { $null }
+    }
+    if($ScanForStartupProceduresDisabled){
+        $StartUpSPs = $Instance.Databases['master'].StoredProcedures.Where{$_. Name -ne 'sp_MSrepl_startup' -and $_.StartUp -eq $true}.count
+        if ($StartUpSPs -eq 0)  {
+            $testInstanceObject.Configuration.ScanForStartupProcedures.ConfigValue = 0
+        } 
+    }
+    return $testInstanceObject
+}
