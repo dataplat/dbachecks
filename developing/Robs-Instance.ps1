7f50060176d9b4992f9d844339cd75c94f9694b4
SQLDbaWithABeard <mrrobsewell@outlook.com>
2022-06-29 07:16:36 +0000

    robs file testing

--- developing/Robs-Instance.ps1
+++ developing/Robs-Instance.ps1
@@ -1,9 +1,12 @@
 $Checks = 'XESessionExists','XESessionStopped','XpCmdShellDisabled','WhoIsActiveInstalled','CLREnabled','TraceFlagsNotExpected','TraceFlagsExpected','TwoDigitYearCutoff','MaxDopInstance','ErrorLogCount','ModelDbGrowth','DefaultBackupCompression','SaExist','SaDisabled','SaRenamed','DefaultFilePath','AdHocDistributedQueriesEnabled','AdHocWorkload',  'DefaultTrace', 'OleAutomationProceduresDisabled', 'CrossDBOwnershipChaining', 'ScanForStartupProceduresDisabled', 'RemoteAccessDisabled', 'SQLMailXPsDisabled', 'DAC', 'OLEAutomation'
-$Checks = 'ErrorLogCount'
+$Checks = 'XESessionExists'
 
-Invoke-PerfAndValidateCheck -Checks $Checks -Verbose 
+Invoke-PerfAndValidateCheck -Checks $Checks 
 
+$password = ConvertTo-SecureString "dbatools.IO" -AsPlainText -Force
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList "sqladmin", $password
 $show = 'All'
+
 $v4code = Invoke-DbcCheck -SqlInstance $Sqlinstances -SqlCredential $cred -Check $Checks -legacy $true -Show $show -PassThru
 # Run v5 checks
-$v5code = Invoke-DbcCheck -SqlInstance $Sqlinstances -SqlCredential $cred -Check $Checks -legacy $false -Show $show -PassThru
+$v5code = Invoke-DbcCheck -SqlInstance $Sqlinstances -SqlCredential $cred -Check $Checks -legacy $false -Show $show -PassThru # -Verbose
